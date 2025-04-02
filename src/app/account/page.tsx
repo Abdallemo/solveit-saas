fc7975b90f91186730d0595d28941caeb2ac1a51
EngCamo <learn3038it@gmail.com>
2025-04-03 03:20:55 +0800

    added required pages

---
+++ src/app/account/page.tsx
@@ -0,0 +1,7 @@
+
+export default  function page() {
+  return (
+    <div>Account </div>
+  )
+}
+
---
+++ src/app/dashboard/moderator/page.tsx
@@ -0,0 +1,7 @@
+
+export default function page() {
+  return (
+    <div>Moderator Dashbaord</div>
+  )
+}
+
