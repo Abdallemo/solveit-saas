2a7294095cf91256659ab3907596eaf0b745531e
Abdalle mohamed <learn3038it@gmail.com>
2025-04-13 06:46:35 +0800

    Update route.ts

--- src/app/api/webhooks/stripe/subscription/checkout/route.ts
+++ src/app/api/webhooks/stripe/subscription/checkout/route.ts
@@ -6,6 +6,11 @@ import { env } from "@/env/server";
 export async function GET() {
   return new Response("Webhook route is active", { status: 200 });
 }
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 export async function POST(request: NextRequest) {
   const sig = request.headers.get("stripe-signature") as string;
