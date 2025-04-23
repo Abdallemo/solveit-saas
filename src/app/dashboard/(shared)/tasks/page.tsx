f87ab92a788108486fef67c81ae12062b899bc4f
EngCamo <learn3038it@gmail.com>
2025-04-23 22:58:32 +0800

    poster UI

--- src/app/dashboard/(shared)/tasks/page.tsx
+++ src/app/dashboard/(shared)/tasks/page.tsx
@@ -1,12 +1,16 @@
+'use client'
 import { Button } from "@/components/ui/button";
 import TaskPostingEditor from "@/features/tasks/components/Tiptap";
+import { useState } from "react";
 
 export default function page() {
+  const [inputValue,setInputValue] = useState<string>("")
+
   return (
     <div className="max-w-3xl mx-auto flex flex-col ">
       <h1>Task/Job posting Page</h1>
 
-      <TaskPostingEditor />
+      <TaskPostingEditor inputValue={inputValue} setInputValue={setInputValue} />
     <div className="flex mt-3 justify-end">
     <Button variant={'success'}>Save</Button>
 
