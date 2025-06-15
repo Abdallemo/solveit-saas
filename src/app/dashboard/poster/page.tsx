f653a376770fe24581d0f0e3daadd37675dcbaa7
EngCamo <learn3038it@gmail.com>
2025-06-15 17:18:51 +0800

    conifgs

--- src/app/dashboard/poster/page.tsx
+++ src/app/dashboard/poster/page.tsx
@@ -3,14 +3,30 @@ import {
   getServerUserSession,
   isAuthorized,
 } from "@/features/auth/server/actions";
-import { getServerUserSubscriptionById } from "@/features/users/server/actions";
+import {
+  getServerUserSubscriptionById,
+  getUserById,
+} from "@/features/users/server/actions";
+import { redirect } from "next/navigation";
 
 export default async function page() {
-  await isAuthorized("POSTER");
   const currentUser = await getServerUserSession();
+
+  if (!currentUser) {
+    return redirect("/api/auth/signout?callbackUrl=/login");
+  }
+
+  const userInDb = await getUserById(currentUser.id!);
+  if (!userInDb) {
+    return redirect(
+      "/api/auth/signout?callbackUrl=/login?error=account_deleted"
+    );
+  }
+  await isAuthorized("POSTER");
   const userRole = await getServerUserSubscriptionById(currentUser?.id);
   userRole;
-  const cache = (Math.random()* 1000)
+  console.log(userRole);
+  const cache = Math.random() * 1000;
   return (
     <main className="flex flex-col w-full h-screen justify-center items-center">
       <p>Dashbard</p>
