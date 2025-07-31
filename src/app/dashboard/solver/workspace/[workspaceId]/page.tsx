8ac3fdc2457f81df1fb84a26914e40364147abf6
EngCamo <learn3038it@gmail.com>
2025-07-31 23:58:29 +0800

    add deadline prevention

--- src/app/dashboard/solver/workspace/[workspaceId]/page.tsx
+++ src/app/dashboard/solver/workspace/[workspaceId]/page.tsx
@@ -1,5 +1,12 @@
 import WorkspacePageComp from "@/features/tasks/components/WorkspacePageComp";
+import { isAuthorized } from "@/features/auth/server/actions";
+import {  getWorkspaceById, handleTaskDeadline} from "@/features/tasks/server/action";
 
-export default async function Page() {
+export default async function Page({params}:{params: Promise<{ workspaceId: string }>;}) {
+  await isAuthorized("SOLVER");
+  const { workspaceId } = await params;
+  const currentWorkspace = await getWorkspaceById(workspaceId);
+  await handleTaskDeadline(currentWorkspace)
   return <WorkspacePageComp />;
 }
+
