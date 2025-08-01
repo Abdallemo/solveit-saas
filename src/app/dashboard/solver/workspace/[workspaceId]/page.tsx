ea18e131054f075293e6985846f0afe31672aa46
EngCamo <learn3038it@gmail.com>
2025-08-02 00:18:06 +0800

    fixed build issues

--- src/app/dashboard/solver/workspace/[workspaceId]/page.tsx
+++ src/app/dashboard/solver/workspace/[workspaceId]/page.tsx
@@ -1,10 +1,6 @@
 import WorkspacePageComp from "@/features/tasks/components/WorkspacePageComp";
 import { isAuthorized } from "@/features/auth/server/actions";
-export default async function Page({
-  params,
-}: {
-  params: { workspaceId: string };
-}) {
+export default async function Page() {
   await isAuthorized("SOLVER");
 
   return <WorkspacePageComp />;
