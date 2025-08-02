bf9eb0b6a873076ad8254c4df37961e603137942
EngCamo <learn3038it@gmail.com>
2025-08-03 01:10:24 +0800

    added solution component

--- src/app/dashboard/tasks/[id]/solutions/[solutionId]/page.tsx
+++ src/app/dashboard/tasks/[id]/solutions/[solutionId]/page.tsx
@@ -1,9 +1,13 @@
 import SolutionPageComps from '@/features/tasks/components/solutionPageComps'
+import { getSolutionById } from '@/features/tasks/server/action'
 import React from 'react'
 
 export default async function SolutionPage({params}:{params:Promise<{solutionId:string}>}) {
   const {solutionId} = await params
+  console.log("solution id :",solutionId)
+  const solution = await getSolutionById(solutionId)
+
   return (
-    <SolutionPageComps/>
+    <SolutionPageComps solution={solution}/>
   )
 }
