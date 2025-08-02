d6f785a4b8f480a6561878919f602463329ca855
EngCamo <learn3038it@gmail.com>
2025-08-03 03:53:50 +0800

    ui update

--- src/app/dashboard/moderator/page.tsx
+++ src/app/dashboard/moderator/page.tsx
@@ -1,7 +1,4 @@
-import { isAuthorized } from "@/features/auth/server/actions"
-
 export default async function page() {
- await isAuthorized('MODERATOR')
   
   return (
     <div>Moderator Dashbaord</div>
