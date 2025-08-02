8987d9ff7f4310db07b39b3f0c2c9236042f02b4
EngCamo <learn3038it@gmail.com>
2025-08-02 20:50:15 +0800

    added some ui related to solution page

--- src/features/tasks/components/richTextEdito/TaskPreview.tsx
+++ src/features/tasks/components/richTextEdito/TaskPreview.tsx
@@ -55,3 +55,51 @@ export default function TaskPreview({ content }: { content: string }) {
     </div>
   );
 }
+export function SolutionPreview({ content }: { content: string }) {
+  const lowlight = createLowlight(common)
+
+  const editor = useEditor({
+    shouldRerenderOnTransaction: false,
+    editable: false,
+    extensions: [
+      StarterKit.configure({
+        bulletList: {
+          HTMLAttributes: {
+            class: "list-disc ml-3",
+          },
+        },
+        orderedList: {
+          HTMLAttributes: {
+            class: "list-decimal ml-3",
+          },
+        },
+      }),
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+      Highlight,
+      CodeBlockLowlight.configure({
+        lowlight,
+        HTMLAttributes: {
+          class: "tiptap-codeblock",
+        },
+      }),
+      Image,
+    ],
+    content: content,
+    immediatelyRender: false,
+    editorProps: {
+      attributes: {
+        class: "w-full h-full p-6 focus:outline-none prose prose-sm max-w-none",
+      },
+    },
+  })
+
+  return (
+    <div className="border rounded-md flex flex-col w-full">
+      <div className="flex-1 overflow-hidden">
+        <EditorContent editor={editor} className="h-full overflow-y-auto" />
+      </div>
+    </div>
+  )
+}
