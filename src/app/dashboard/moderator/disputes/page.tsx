067ec3a61f68f9b77b3342a34031470b89a07482
EngCamo <learn3038it@gmail.com>
2025-08-05 06:27:56 +0800

    aded disput calls

--- src/app/dashboard/moderator/disputes/page.tsx
+++ src/app/dashboard/moderator/disputes/page.tsx
@@ -1,7 +1,18 @@
+import { Table, TableBody } from '@/components/ui/table'
+import { Columns, RefundTable } from '@/features/tasks/components/refund/refundComps'
+import { getAllDisputes } from '@/features/tasks/server/action'
+import { ColumnDef } from "@tanstack/react-table"
 import React from 'react'
 
-export default function Page() {
+export default async function Page() {
+  const allDisputes = await getAllDisputes()
   return (
-    <div>page</div>
+    <main className='w-full h-full flex  justify-center '>
+      <div className='w-4/5 my-10'>
+        <RefundTable columns={Columns} data={allDisputes}/>
+      </div>
+      
+    </main>
+
   )
 }
