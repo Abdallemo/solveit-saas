2220dbf8929db4cb6ef1ebbeab8f55a0055a9ae4
EngCamo <learn3038it@gmail.com>
2025-08-05 08:00:54 +0800

    Ui enhacnement

--- src/features/tasks/components/taskStatusBadge.tsx
+++ src/features/tasks/components/taskStatusBadge.tsx
@@ -1,17 +1,20 @@
 import { Badge } from "@/components/ui/badge";
 import { RefundStatusEnumType, TaskStatusType } from "@/drizzle/schemas";
+import { AlertCircle, CheckCircle, Clock, Eye } from "lucide-react";
 
 export default function GetStatusBadge(status: TaskStatusType) {
   switch (status) {
     case "OPEN":
       return (
         <Badge variant="secondary" className="bg-green-100 text-green-800">
-          Open
+          <Eye className="w-4 h-4"/>
+          
         </Badge>
       );
     case "IN_PROGRESS":
       return (
         <Badge variant="secondary" className="bg-yellow-100 text-yellow-800">
+          <Clock className="w-4 h-4" />
           In Progress
         </Badge>
       );
@@ -24,17 +27,19 @@ export default function GetStatusBadge(status: TaskStatusType) {
     case "SUBMITTED":
       return (
         <Badge variant="secondary" className="bg-gray-100 text-gray-800">
+          <AlertCircle className="w-4 h-4" />
           Submited
         </Badge>
       );
     case "COMPLETED":
       return (
         <Badge variant="secondary" className="bg-gray-100 text-gray-800">
+          <CheckCircle className="w-4 h-4" />
           Completed
         </Badge>
       );
     default:
-      return <Badge variant="secondary">Unknown</Badge>;
+      return <Badge variant="secondary"><Clock className="w-4 h-4" /> Unknown</Badge>;
   }
 }
 export function GetRefundStatusBadge(status: RefundStatusEnumType) {
