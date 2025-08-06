cab21dfcd31d8923970efb45667ef72b4d3d04a4
EngCamo <learn3038it@gmail.com>
2025-08-07 04:32:41 +0800

    fully changed to server api file uplaod

--- src/app/api/media/route.ts
+++ src/app/api/media/route.ts
@@ -3,40 +3,55 @@ import { NextRequest, NextResponse } from "next/server";
 import { s3 } from "@/lib/cloudFlairR2";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { randomUUID } from "crypto";
+import { scope, UploadedFileMeta } from "@/features/media/server/media-types";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 export async function POST(req: NextRequest) {
   const formData = await req.formData();
-  const file = formData.get("file") as File;
+  const files = formData.getAll("files");
+  const scope = formData.get("scope") as scope
 
-  if (!file) {
-    return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
-  }
+  const uploadedFileDetails:UploadedFileMeta[] = [];
 
-  const fileBuffer = Buffer.from(await file.arrayBuffer());
-  const fileType = file.type || "application/octet-stream";
-  const fileName = `${randomUUID()}-${file.name}`;
+  for (const file of files) {
+    if (!(file instanceof File)) {
+      console.warn("Skipping non-file entry in formData:", file);
+      continue;
+    }
 
-  const command = new PutObjectCommand({
-    Bucket: "solveit",
-    Key: `workspace/${fileName}`,
-    Body: fileBuffer,
-    ContentType: fileType,
-  });
-
-  try {
-    await s3.send(command);
-    const publicUrl = `https://pub-c60addcb244c4d23b18a98d686f3195e.r2.dev/solveit/workspace/${fileName}`;
-
-    return NextResponse.json({
-      success: true,
-      fileName: file.name,
-      publicUrl,
-      filePath: `workspace/${fileName}`,
+    const fileBuffer = Buffer.from(await file.arrayBuffer());
+    const fileType = file.type || "application/octet-stream";
+    const fileName = `${randomUUID()}-${file.name}`; 
+    const filePath = `${scope}/${fileName}`
+    const command = new PutObjectCommand({
+      Bucket: "solveit", 
+      Key: filePath,
+      Body: fileBuffer,
+      ContentType: fileType,
     });
-  } catch (err) {
-    console.error("Upload error:", err);
-    return NextResponse.json({ error: "Upload failed" }, { status: 500 });
+
+    try {
+      await s3.send(command);
+      const publicUrl = `https://pub-c60addcb244c4d23b18a98d686f3195e.r2.dev/solveit/${filePath}`;
+      uploadedFileDetails.push({
+        fileName: file.name,
+        fileSize:file.size,
+        fileType:file.type,
+        storageLocation:publicUrl,
+        filePath: filePath,
+      });
+    } catch (err) {
+      console.error(`Upload error for file ${file.name}:`, err);
+      return NextResponse.json({ error: `Upload failed for ${file.name}` }, { status: 500 });
+    }
   }
+
+  return NextResponse.json(uploadedFileDetails);
 }
 export async function PUT(req: NextRequest) {
   console.log("hit endpoint file update..")
