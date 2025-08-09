f7a2c5a276b6bc56d03f7697679f5ad1b4842ea9
EngCamo <learn3038it@gmail.com>
2025-08-09 23:18:49 +0800

    go changes

--- server/api.go
+++ server/api.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -13,6 +14,7 @@ type APIServer struct {
 	addr string
 	db   *sql.DB
 }
+
 type FileMeta struct {
 	FileName        string  `json:"file_name"`
 	FileType        string  `json:"file_type"`
@@ -22,59 +24,62 @@ type FileMeta struct {
 }
 
 func NewAPIServer(addr string, db *sql.DB) *APIServer {
-	return &APIServer{
-		addr: addr,
-		db:   db,
-	}
+	return &APIServer{addr: addr, db: db}
 }
-
-type Media struct{}
-
-
 
 func (s *APIServer) Run() error {
 	router := http.NewServeMux()
-	router.HandleFunc("POST /api/v1/media", uploadMedia)
+	router.HandleFunc("POST /api/v1/media", s.handleUploadMedia)
 
+	log.Printf("Server listening at %s\n", s.addr)
 	return http.ListenAndServe(s.addr, router)
 }
 
-func uploadMedia(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "multipart/form-data")
-	err := r.ParseMultipartForm(10 << 20)
+func (s *APIServer) handleUploadMedia(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(10 << 20) //10 * (2^20)
 	if err != nil {
 		http.Error(w, "Unable to parse form", http.StatusBadRequest)
 		return
 	}
 
 	files := r.MultipartForm.File["files"]
-	uploadedFile := []Media{}
+	var uploadedFiles []FileMeta
+
 	for _, fileHeader := range files {
 		file, err := fileHeader.Open()
 		if err != nil {
+			log.Println("file open error:", err)
 			continue
 		}
-		defer file.Close()
-		savePath := filepath.Join("uploaded",fileHeader.Filename)
 
-		dst, er := os.Create("./uploaded/" + fileHeader.Filename)
-		if er != nil {
-
-		}
-		defer dst.Close()
-		size, erro:=io.Copy(dst, file)
-		if erro != nil{
+		savePath := filepath.Join("uploaded", fileHeader.Filename)
+		dst, err := os.Create(savePath)
+		if err != nil {
+			log.Println("file create error:", err)
+			file.Close()
 			continue
 		}
-		fileMeta := FileMeta{
-			FileName: fileHeader.Filename,
-			FileType: fileHeader.Header[][],
+
+		size, err := io.Copy(dst, file)
+		if err != nil {
+			log.Println("file save error:", err)
 		}
+
+		file.Close()
+		dst.Close()
+
+		meta := FileMeta{
+			FileName:        fileHeader.Filename,
+			FileType:        fileHeader.Header.Get("Content-Type"),
+			FileSize:        float64(size),
+			FilePath:        "/" + savePath,
+			StorageLocation: "local",
+		}
+
+		log.Printf("Uploaded: %+v\n", meta)
+		uploadedFiles = append(uploadedFiles, meta)
 	}
+
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{
-		"message": "Files uploaded successfully",
-
-	})
-
+	json.NewEncoder(w).Encode(uploadedFiles)
 }
