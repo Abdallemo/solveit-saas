3b9c0ed68e115717063c056ed2eceb89678e1004
EngCamo <learn3038it@gmail.com>
2025-08-11 14:27:29 +0800

    setuped main.go to initilize s3

--- server/main.go
+++ server/main.go
@@ -1,10 +1,39 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	server := NewWsNotification()
-	log.Fatal(server.run())
+	if err := godotenv.Load(filepath.Join("..", ".env")); err != nil {
+		log.Println("No .env file found, falling back to system env")
+	}
+	cfg, err := config.LoadDefaultConfig(context.Background(),
+		config.WithRegion("auto"),
+		config.WithCredentialsProvider(
+			credentials.NewStaticCredentialsProvider(
+				os.Getenv("S3_ACCESS_KEY_ID"),
+				os.Getenv("S3_SECRTE_ACCESS_KEY_ID"),
+				"",
+			),
+		),
+	)
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	s3Client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.BaseEndpoint = aws.String(os.Getenv("S3_ENDPOINT"))
+	})
+	server := NewServer(":3030", s3Client)
+	log.Fatal(server.Run())
 }
