18db4430943e189249ac71c4bd7a81b3e2df4680
EngCamo <learn3038it@gmail.com>
2025-08-11 14:30:02 +0800

    added both server and client envs

--- src/env/client.ts
+++ src/env/client.ts
@@ -3,9 +3,13 @@ import {z} from 'zod'
 export const env = createEnv({
     emptyStringAsUndefined:true,
     client:{
-        NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:z.string()
+        NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:z.string(),
+        NEXT_PUBLIC_GO_API_URL:z.string(),
+        NEXT_PUBLIC_GO_API_WS_URL:z.string(),
     },
     runtimeEnv:{
-        NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+        NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+        NEXT_PUBLIC_GO_API_URL:process.env.NEXT_PUBLIC_GO_API_URL,
+        NEXT_PUBLIC_GO_API_WS_URL:process.env.NEXT_PUBLIC_GO_API_WS_URL,
     },
 })
