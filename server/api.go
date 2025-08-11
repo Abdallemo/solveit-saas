ba63b3e269fcae2398d378f86684a93d51d6d078
EngCamo <learn3038it@gmail.com>
2025-08-11 14:26:33 +0800

    setuped api.go to handle both media upload and websocket

--- server/api.go
+++ server/api.go
@@ -1,85 +1,42 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
-	"io"
 	"log"
 	"net/http"
-	"os"
-	"path/filepath"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-type APIServer struct {
+type Server struct {
 	addr string
-	db   *sql.DB
+	// db       *sql.DB //dont need it now
+	ws       *wsNotification
+	s3Client *s3.Client //
 }
 
-type FileMeta struct {
-	FileName        string  `json:"file_name"`
-	FileType        string  `json:"file_type"`
-	FileSize        float64 `json:"file_size"`
-	FilePath        string  `json:"file_path"`
-	StorageLocation string  `json:"file_location"`
-}
-
-func NewAPIServer(addr string, db *sql.DB) *APIServer {
-	return &APIServer{addr: addr, db: db}
-}
-
-func (s *APIServer) Run() error {
-	router := http.NewServeMux()
-	router.HandleFunc("POST /api/v1/media", s.handleUploadMedia)
-
-	log.Printf("Server listening at %s\n", s.addr)
-	return http.ListenAndServe(s.addr, router)
-}
-
-func (s *APIServer) handleUploadMedia(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseMultipartForm(10 << 20) //10 * (2^20)
-	if err != nil {
-		http.Error(w, "Unable to parse form", http.StatusBadRequest)
-		return
+func NewServer(addr string, s3Client *s3.Client) *Server {
+	return &Server{
+		addr: addr,
+		ws:   NewWsNotification(),
+		// db:   db,
+		s3Client: s3Client,
 	}
+}
 
-	files := r.MultipartForm.File["files"]
-	var uploadedFiles []FileMeta
+func (s *Server) routes() *http.ServeMux {
+	mux := http.NewServeMux()
 
-	for _, fileHeader := range files {
-		file, err := fileHeader.Open()
-		if err != nil {
-			log.Println("file open error:", err)
-			continue
-		}
+	// WebSocket endpoints
+	mux.HandleFunc("/api/v1/notification", s.ws.handleNotification)
+	mux.HandleFunc("POST /api/v1/send-notification", s.ws.handleSendNotification)
 
-		savePath := filepath.Join("uploaded", fileHeader.Filename)
-		dst, err := os.Create(savePath)
-		if err != nil {
-			log.Println("file create error:", err)
-			file.Close()
-			continue
-		}
+	// Media upload endpoint
+	mux.HandleFunc("POST /api/v1/media", s.handleUploadMedia)
+	mux.HandleFunc("DELETE /api/v1/media", s.handleDeleteMedia)
+	return mux
+}
 
-		size, err := io.Copy(dst, file)
-		if err != nil {
-			log.Println("file save error:", err)
-		}
-
-		file.Close()
-		dst.Close()
-
-		meta := FileMeta{
-			FileName:        fileHeader.Filename,
-			FileType:        fileHeader.Header.Get("Content-Type"),
-			FileSize:        float64(size),
-			FilePath:        "/" + savePath,
-			StorageLocation: "local",
-		}
-
-		log.Printf("Uploaded: %+v\n", meta)
-		uploadedFiles = append(uploadedFiles, meta)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(uploadedFiles)
+func (s *Server) Run() error {
+	log.Printf("server running on port:%s", s.addr)
+	return http.ListenAndServe(s.addr, s.routes())
 }
