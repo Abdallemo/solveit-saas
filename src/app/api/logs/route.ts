ad2a9402f28f0c7d53bdaab28ac54ee96626533c
EngCamo <learn3038it@gmail.com>
2025-08-11 19:32:41 +0800

    created logger api endpoint

---
+++ src/app/api/logs/route.ts
@@ -0,0 +1,21 @@
+import db from "@/drizzle/db";
+import { logTable } from "@/drizzle/schemas";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  const level: string = body.level;
+  const timestamp = body.timestamp;
+  const message: string = body.message;
+  const error: string = body.error;
+
+  await db.insert(logTable).values({
+    timestamp: new Date(timestamp),
+    level: level,
+    message: message,
+    error: error ? JSON.stringify(body.error) : null,
+  });
+  return new Response("Logged");
+}
+export async function GET(req: NextRequest) {}
