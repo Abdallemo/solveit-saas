5e9318eaa9dc6b8dfabde986aa0d5973aa2b7d50
EngCamo <learn3038it@gmail.com>
2025-08-11 22:08:12 +0800

    ticker added

--- server/cmd/api/websocket.go
+++ server/cmd/api/websocket.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -79,7 +80,18 @@ func (s *wsNotification) handleNotification(w http.ResponseWriter, r *http.Reque
 	s.mu.Lock()
 	s.conns[userID] = append(s.conns[userID], conn)
 	s.mu.Unlock()
-
+	go func() {
+		ticker := time.NewTicker(30 * time.Second)
+		defer ticker.Stop()
+		for {
+			<-ticker.C
+			conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+			if err := conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				log.Println("ping error:", err)
+				return
+			}
+		}
+	}()
 	go s.cleanUp(conn, userID)
 	fmt.Println("new Connection from client:", conn.RemoteAddr())
 	for usrid := range s.conns {
