c2df01f0e115d667fc097fb570a6fe3973b1fd90
EngCamo <learn3038it@gmail.com>
2025-08-12 23:28:15 +0800

    .

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -2,11 +2,12 @@ import { isAuthorized } from "@/features/auth/server/actions";
 import AdminDashboardComponent from "@/features/users/components/admin/AdminHomePageComponent";
 import { getSummaryStats } from "@/features/users/server/actions";
 import { getLogs } from "@/lib/logging/action";
-
+export type StastType = Awaited<ReturnType<typeof getSummaryStats>>
 export default async function page() {
   await isAuthorized("ADMIN");
   const serverLogs = await getLogs();
-  const users = await getSummaryStats();
+  const stats = await getSummaryStats();
   
-  return <AdminDashboardComponent serverLogs={serverLogs} />;
+  
+  return <AdminDashboardComponent serverLogs={serverLogs} stats ={stats} />;
 }
