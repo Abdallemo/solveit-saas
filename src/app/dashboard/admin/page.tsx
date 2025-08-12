dde6ba1355efeebe48dd55fb8c4fb8844fb5088e
EngCamo <learn3038it@gmail.com>
2025-08-12 23:25:42 +0800

    ui update

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,11 +1,12 @@
 import { isAuthorized } from "@/features/auth/server/actions";
 import AdminDashboardComponent from "@/features/users/components/admin/AdminHomePageComponent";
+import { getSummaryStats } from "@/features/users/server/actions";
 import { getLogs } from "@/lib/logging/action";
 
 export default async function page() {
   await isAuthorized("ADMIN");
-  const serverLogs = await getLogs()
-  return (
-    <AdminDashboardComponent serverLogs={serverLogs}/>
-  );
+  const serverLogs = await getLogs();
+  const users = await getSummaryStats();
+  
+  return <AdminDashboardComponent serverLogs={serverLogs} />;
 }
