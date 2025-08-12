0e8942ad0d3b4d54939cc4c49091b34ac4548737
EngCamo <learn3038it@gmail.com>
2025-08-13 06:22:53 +0800

    added subscrption from stripe

---
+++ src/app/dashboard/admin/subscriptions/page.tsx
@@ -0,0 +1,11 @@
+import { SubscriptionManagement } from "@/features/subscriptions/components/SubscriptionManagement";
+import { getAllSubscriptions } from "@/features/subscriptions/server/action";
+
+export default async function page() {
+  const subscriptions = await getAllSubscriptions()
+  
+  return (
+
+    <SubscriptionManagement subscription={subscriptions} />
+  )
+}
