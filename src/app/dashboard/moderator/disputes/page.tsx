8c8e4e87e938fbebdaeb971b0d397a9b9d9d1f62
EngCamo <learn3038it@gmail.com>
2025-08-13 06:23:02 +0800

    ui update

--- src/app/dashboard/moderator/disputes/page.tsx
+++ src/app/dashboard/moderator/disputes/page.tsx
@@ -8,7 +8,11 @@ export default async function Page() {
   const allDisputes = await getAllDisputes()
   return (
     <main className='w-full h-full flex  justify-center '>
-      <div className='w-4/5 my-10'>
+      <div className='container mx-auto py-6 px-4'>
+       <h1 className="text-3xl font-bold tracking-tight">Dispute Management</h1>
+          <p className="text-muted-foreground">
+            Manage All poster disputes here
+          </p>
         <RefundTable columns={Columns} data={allDisputes}/>
       </div>
       
