077b6618eadef1676db60d0f0acd29824144df73
EngCamo <learn3038it@gmail.com>
2025-08-13 07:07:36 +0800

    some updates

--- src/app/dashboard/admin/subscriptions/page.tsx
+++ src/app/dashboard/admin/subscriptions/page.tsx
@@ -1,8 +1,9 @@
+export const revalidate = 300;
 import { SubscriptionManagement } from "@/features/subscriptions/components/SubscriptionManagement";
-import { getAllSubscriptions } from "@/features/subscriptions/server/action";
+import { getAllSubscriptions, getAllSubscriptionsCached } from "@/features/subscriptions/server/action";
 
 export default async function page() {
-  const subscriptions = await getAllSubscriptions()
+  const subscriptions = await getAllSubscriptionsCached()
   
   return (
 
