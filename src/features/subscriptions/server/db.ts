0d59f2058ec8cc133a48622428ed0d937c25eb6c
EngCamo <learn3038it@gmail.com>
2025-08-14 06:13:49 +0800

    updated updateUserSubscription to support downgrade and upgrade

--- src/features/subscriptions/server/db.ts
+++ src/features/subscriptions/server/db.ts
@@ -1,20 +1,39 @@
 import db from "@/drizzle/db";
 import { eq, SQL } from "drizzle-orm";
 import { UserRole } from "../../../../types/next-auth";
-import {  UserTable, UserSubscriptionTable } from "@/drizzle/schemas";
+import { UserTable, UserSubscriptionTable } from "@/drizzle/schemas";
+import { logger } from "@/lib/logging/winston";
 
 export async function updateUserSubscription(
   values: typeof UserSubscriptionTable.$inferInsert,
-  role: UserRole = "SOLVER",
+  role: UserRole,
   id: string
 ) {
-  await db.transaction(async (dx) => {
-    await dx
-      .update(UserSubscriptionTable)
-      .set(values)
-      .where(eq(UserSubscriptionTable.userId, id));
-    await dx.update(UserTable).set({ role: role }).where(eq(UserTable.id, id));
-  });
+  try {
+    await db.transaction(async (dx) => {
+      await dx
+        .update(UserSubscriptionTable)
+        .set(values)
+        .where(eq(UserSubscriptionTable.userId, id));
+      await dx
+        .update(UserTable)
+        .set({ role: role })
+        .where(eq(UserTable.id, id));
+    });
+    logger.info(`Sucessfully Updated User ${id}'s Subscription`)
+  } catch (error) {
+    logger.error(
+      "unable to update user subscription cause: ",
+      (error as Error).message,
+      {
+        message: (error as Error).message,
+        cause: (error as Error).cause,
+      }
+    );
+    throw new Error("unable to update user subscription cause: ", {
+      cause: error,
+    });
+  }
 }
 
 export async function CancelUserSubscription(
@@ -25,7 +44,10 @@ export async function CancelUserSubscription(
   await db.transaction(async (dx) => {
     await dx.update(UserSubscriptionTable).set(values).where(where);
 
-    await dx.update(UserTable).set({ role: "POSTER" }).where(eq(UserTable.id, id!));
+    await dx
+      .update(UserTable)
+      .set({ role: "POSTER" })
+      .where(eq(UserTable.id, id!));
   });
 }
 
