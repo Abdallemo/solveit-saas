766a6b26a4bd8f3194294147ccc79ed9b16ec323
EngCamo <learn3038it@gmail.com>
2025-08-14 06:15:12 +0800

    added SubMap which is a in mem runtime map for stripe tiers

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,5 +1,11 @@
 'server-only'
 
+import { TierType } from '@/drizzle/schemas'
+import { env } from '@/env/server'
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
+export const SubMap:Record<string,TierType>={
+  [env.STRIPE_SOLVER_PRODUCT_ID]:"SOLVER",
+  [env.STRIPE_SOLVER_PLUS_PRICE_ID]:"SOLVER++",
+}
