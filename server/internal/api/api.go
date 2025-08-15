d438191e3109f8c4dd13017e2b785d8464c27d33
EngCamo <learn3038it@gmail.com>
2025-08-15 16:27:34 +0800

    configured the api to be repos pattern

--- server/internal/api/api.go
+++ server/internal/api/api.go
@@ -1,47 +1,64 @@
 package api
 
 import (
+	"github/abdallemo/solveit-saas/internal/middleware"
+	"github/abdallemo/solveit-saas/internal/storage"
 	"log"
 	"net/http"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/sashabaranov/go-openai"
 )
 
 type Server struct {
-	addr     string
-	hub      *WsHub
-	wsNotif  *wsNotification
-	wsComm   *wsComments
-	s3Client *s3.Client
+	addr         string
+	hub          *WsHub
+	wsNotif      *wsNotification
+	wsComm       *wsComments
+	s3Client     *s3.Client
+	openaiClient *openai.Client
+	store        storage.Storage
 }
 
-func NewServer(addr string, s3Client *s3.Client) *Server {
+func NewServer(addr string, s3Client *s3.Client, openaiClient *openai.Client, store storage.Storage) *Server {
 	hub := NewHub()
 
 	return &Server{
-		addr:     addr,
-		hub:      hub,
-		wsNotif:  NewWsNotification(hub),
-		wsComm:   NewWsComments(hub),
-		s3Client: s3Client,
+		addr:         addr,
+		hub:          hub,
+		wsNotif:      NewWsNotification(hub),
+		wsComm:       NewWsComments(hub),
+		s3Client:     s3Client,
+		openaiClient: openaiClient,
+		store:        store,
 	}
 }
 
 func (s *Server) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	// WebSocket endpoints
+	s.registerPublicRoutes(mux)
+
+	securedMux := http.NewServeMux()
+	s.registerSecuredRoutes(securedMux)
+
+	authStack := middleware.CreateStack(middleware.IsAuthorized)
+	mux.Handle("/", authStack(securedMux))
+
+	return mux
+}
+
+func (s *Server) registerPublicRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("GET /api/v1/notification", s.wsNotif.handleNotification)
-	mux.HandleFunc("POST /api/v1/send-notification", s.wsNotif.handleSendNotification)
-
-	// comments WebSocket endpoints
 	mux.HandleFunc("GET /api/v1/comments", s.wsComm.handleComments)
-	mux.HandleFunc("POST /api/v1/send-comment", s.wsComm.handleSendComments)
+}
 
-	// Media upload endpoint
+func (s *Server) registerSecuredRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("POST /api/v1/media", s.handleUploadMedia)
 	mux.HandleFunc("DELETE /api/v1/media", s.handleDeleteMedia)
-	return mux
+	mux.HandleFunc("POST /api/v1/send-notification", s.wsNotif.handleSendNotification)
+	mux.HandleFunc("POST /api/v1/send-comment", s.wsComm.handleSendComments)
+	mux.HandleFunc("GET /api/v1/openai", s.hanleOpenAi)
 }
 
 func (s *Server) Run() error {
