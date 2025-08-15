758389b6e8492282268394aa4cddeaa4d43c0c5d
EngCamo <learn3038it@gmail.com>
2025-08-15 13:13:32 +0800

    added comment socket

--- server/internal/api/api.go
+++ server/internal/api/api.go
@@ -8,17 +8,21 @@ import (
 )
 
 type Server struct {
-	addr string
-	// db       *sql.DB //dont need it now
-	ws       *wsNotification
-	s3Client *s3.Client //
+	addr     string
+	hub      *WsHub
+	wsNotif  *wsNotification
+	wsComm   *wsComments
+	s3Client *s3.Client
 }
 
 func NewServer(addr string, s3Client *s3.Client) *Server {
+	hub := NewHub()
+
 	return &Server{
-		addr: addr,
-		ws:   NewWsNotification(),
-		// db:   db,
+		addr:     addr,
+		hub:      hub,
+		wsNotif:  NewWsNotification(hub),
+		wsComm:   NewWsComments(hub),
 		s3Client: s3Client,
 	}
 }
@@ -27,8 +31,12 @@ func (s *Server) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	// WebSocket endpoints
-	mux.HandleFunc("GET /api/v1/notification", s.ws.handleNotification)
-	mux.HandleFunc("POST /api/v1/send-notification", s.ws.handleSendNotification)
+	mux.HandleFunc("GET /api/v1/notification", s.wsNotif.handleNotification)
+	mux.HandleFunc("POST /api/v1/send-notification", s.wsNotif.handleSendNotification)
+
+	// comments WebSocket endpoints
+	mux.HandleFunc("GET /api/v1/comments", s.wsComm.handleComments)
+	mux.HandleFunc("POST /api/v1/send-comment", s.wsComm.handleSendComments)
 
 	// Media upload endpoint
 	mux.HandleFunc("POST /api/v1/media", s.handleUploadMedia)
