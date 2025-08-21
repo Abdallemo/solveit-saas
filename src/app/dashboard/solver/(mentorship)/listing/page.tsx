ea0711d39e3d39338ac70e4a03c5302e8165a36e
EngCamo <learn3038it@gmail.com>
2025-08-21 21:08:29 +0800

    created mentor listing page

---
+++ src/app/dashboard/solver/(mentorship)/listing/page.tsx
@@ -0,0 +1,22 @@
+import { MentorProfile } from "@/features/mentore/components/componentMentorListing";
+import { getMentorListig, MentorListType } from "@/features/mentore/server/action";
+
+export default async function MentorPage() {
+  let mentorData:MentorListType
+  try {
+    mentorData = await getMentorListig("solver++") as MentorListType
+  } catch (error) {}
+  return (
+    <div className="min-h-full bg-background">
+      <div className="container mx-auto px-4 py-8">
+        <div className="mb-8">
+          <h1 className="text-3xl font-bold tracking-tight">Mentor Profile</h1>
+          <p className="text-muted-foreground mt-2">
+            Manage your mentor profile and availability for mentees
+          </p>
+        </div>
+        <MentorProfile MentorData1={mentorData}/>
+      </div>
+    </div>
+  );
+}
