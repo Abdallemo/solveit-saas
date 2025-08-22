f1bbf51eca35e5f204657d7cbb17cf0a884cdb52
EngCamo <learn3038it@gmail.com>
2025-08-23 07:58:44 +0800

    configured poster task to recieave toast notification after success

--- src/app/dashboard/poster/yourTasks/page.tsx
+++ src/app/dashboard/poster/yourTasks/page.tsx
@@ -1,43 +1,52 @@
-import { getServerUserSession } from "@/features/auth/server/actions"
-import { getAllCategoryMap, getPosterTasksbyIdPaginated } from "@/features/tasks/server/action"
+import { getServerUserSession } from "@/features/auth/server/actions";
+import {
+  getAllCategoryMap,
+  getPosterTasksbyIdPaginated,
+} from "@/features/tasks/server/action";
 import DisplayListComponent from "@/features/tasks/components/DisplayComponent";
-
+import StripeCheckoutSucessClient from "@/features/tasks/components/stripeCheckoutSucessClient";
 
 export default async function ServerWrapper({
   searchParams,
 }: {
-  searchParams: Promise<{ q: string; page: string }>
+  searchParams: Promise<{ q: string; page: string; id: string }>;
 }) {
-  const currentUser = await getServerUserSession()
-  if (!currentUser || !currentUser.role || !currentUser.id) return
+  const currentUser = await getServerUserSession();
+  if (!currentUser || !currentUser.role || !currentUser.id) return;
 
-  const categoryMap = await getAllCategoryMap()
-  const { q, page } = await searchParams
-  const search = q ?? ""
-  const pages = Number.parseInt(page ?? "1")
-  const limit = 6
-  const offset = (pages - 1) * limit
+  const categoryMap = await getAllCategoryMap();
+  const { q, page, id } = await searchParams;
+  const search = q ?? "";
+  const pages = Number.parseInt(page ?? "1");
+  const limit = 6;
+  const offset = (pages - 1) * limit;
 
-  const { tasks, totalCount } = await getPosterTasksbyIdPaginated(currentUser.id, {
-    search,
-    limit,
-    offset,
-  })
+  const { tasks, totalCount } = await getPosterTasksbyIdPaginated(
+    currentUser.id,
+    {
+      search,
+      limit,
+      offset,
+    }
+  );
 
-  const totalPages = Math.ceil(totalCount / limit)
-  const hasPrevious = pages > 1
-  const hasNext = pages < totalPages
+  const totalPages = Math.ceil(totalCount / limit);
+  const hasPrevious = pages > 1;
+  const hasNext = pages < totalPages;
   return (
-    <DisplayListComponent
-      title="Your Tasks"
-      itretable={tasks}
-      totalCount={totalCount}
-      categoryMap={categoryMap}
-      pages={pages}
-      totalPages={totalPages}
-      hasPrevious={hasPrevious}
-      hasNext={hasNext}
-      filterType="status"
-    />
-  )
+    <>
+      <StripeCheckoutSucessClient id={id} />
+      <DisplayListComponent
+        title="Your Tasks"
+        itretable={tasks}
+        totalCount={totalCount}
+        categoryMap={categoryMap}
+        pages={pages}
+        totalPages={totalPages}
+        hasPrevious={hasPrevious}
+        hasNext={hasNext}
+        filterType="status"
+      />
+    </>
+  );
 }
