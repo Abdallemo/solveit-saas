3f33b4ed0f304fe234b79615b8c6daf3b92f88a0
EngCamo <learn3038it@gmail.com>
2025-08-23 01:04:42 +0800

    fixed future stripe bugs for task creation

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -21,6 +21,7 @@ import type Stripe from "stripe";
 import { logger } from "@/lib/logging/winston";
 import { getUserById, UpdateUserField } from "@/features/users/server/actions";
 import { getServerUserSession } from "@/features/auth/server/actions";
+import { getPaymentByPaymentIntentId } from "@/features/payments/server/action";
 
 export async function GET() {
   return new Response("Webhook route is active", { status: 200 });
@@ -32,44 +33,52 @@ export const config = {
 };
 
 export async function POST(request: NextRequest) {
-  const sig = request.headers.get("stripe-signature") as string;
-  const secret = env.STRIPE_WEBHOOK_SECRET;
-  const body = await request.text();
-  const event = stripe.webhooks.constructEvent(body, sig, secret);
-  const userSession = await getServerUserSession();
-  switch (event.type) {
-    case "checkout.session.completed":
-      await handleTaskCreate(event);
-      break;
-    case "payment_method.updated":
-      console.log(event);
-      const paymentMethod = event.data.object;
-      const res = await stripe.paymentMethods.update(paymentMethod.id, {
-        allow_redisplay: "always",
-        billing_details: {
-          name: userSession?.name!,
-          email: userSession?.email!,
-        },
-      });
+  try {
+    const sig = request.headers.get("stripe-signature") as string;
+    const secret = env.STRIPE_WEBHOOK_SECRET;
+    const body = await request.text();
+    const event = stripe.webhooks.constructEvent(body, sig, secret);
+    const userSession = await getServerUserSession();
+    switch (event.type) {
+      case "checkout.session.completed":
+        await handleTaskCreate(event);
+        break;
+      case "payment_method.updated":
+        console.log(event);
+        const paymentMethod = event.data.object;
+        const res = await stripe.paymentMethods.update(paymentMethod.id, {
+          allow_redisplay: "always",
+          billing_details: {
+            name: userSession?.name!,
+            email: userSession?.email!,
+          },
+        });
 
-      console.log("res", res);
-      break;
-    case "customer.subscription.updated":
-      await handleUpdate(event.data.object);
-      break;
-    case "customer.subscription.created":
-      await handleCreate(event.data.object);
-      break;
+        console.log("res", res);
+        break;
+      case "customer.subscription.updated":
+        await handleUpdate(event.data.object);
+        break;
+      case "customer.subscription.created":
+        await handleCreate(event.data.object);
+        break;
 
-    case "customer.subscription.deleted":
-      await handleDelete(event.data.object);
-      break;
+      case "customer.subscription.deleted":
+        await handleDelete(event.data.object);
+        break;
 
-    default:
-      logger.debug(`Unhandled event type ${event.type}`);
-      break;
+      default:
+        logger.debug(`Unhandled event type ${event.type}`);
+        break;
+    }
+    return new Response(null, { status: 200 });
+  } catch (error) {
+    logger.error("internal Error on Stripe Webhook", {
+      message: (error as Error).message,
+      stack: (error as Error).stack,
+    });
+    return new Response(null, { status: 500 });
   }
-  return new Response(null, { status: 200 });
 }
 
 async function handleCreate(subscription: string | Stripe.Subscription) {
@@ -160,21 +169,39 @@ async function handleTaskCreate(event: Stripe.Event) {
       typeof paymentIntent.customer === "string"
         ? paymentIntent.customer
         : paymentIntent.customer?.id;
-    logger.info("found userId:" + userId);
-    if (!userId) return;
+
+    
+
+    if (!userId) {
+      logger.warn("couldnt found userid: " + userId);
+      return;
+    }
+
     const paymentMethodId =
       typeof paymentIntent.payment_method === "string"
         ? paymentIntent.payment_method
         : paymentIntent.payment_method?.id;
+
     await UpdateUserField({
       id: userId,
       data: { stripeCustomerId: customerId },
     });
+
     logger.info(`paymentMethodId for user ${userId} is : ${paymentMethodId}`);
+
     await stripe.paymentMethods.update(paymentMethodId!, {
       allow_redisplay: "always",
     });
     const amount = session.amount_total! / 100;
+    const paymentExist = await getPaymentByPaymentIntentId(paymentIntentId);
+
+    if (paymentExist) {
+      logger.warn(
+        `PaymentIntent ${paymentIntentId} already processed, skipping`
+      );
+      return;
+    }
+
     const paymentId = await taskPaymentInsetion(
       "HOLD",
       amount,
@@ -182,10 +209,16 @@ async function handleTaskCreate(event: Stripe.Event) {
       "Task Payment",
       paymentIntentId
     );
-    if (!paymentId) return;
+    if (!paymentId) {
+      logger.warn("unable to insert paymentIntent for the payment table ")
+      return
+    };
 
     const draftTasks = await getDraftTask(userId);
-    if (!draftTasks) return;
+    if (!draftTasks) {
+      logger.warn("unable to find any draft task for this user: "+userId)
+      return;
+    }
 
     const {
       title,
@@ -225,5 +258,8 @@ async function handleTaskCreate(event: Stripe.Event) {
       error: error,
       stripeEventId: event.id,
     });
+    throw new Error(
+      "Task Handle Webhook Failed due to: " + (error as Error).message
+    );
   }
 }
