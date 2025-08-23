d70697f026d37ac1149f4e5884b59a8a41ab45c7
EngCamo <learn3038it@gmail.com>
2025-08-23 08:05:01 +0800

    new task provider a migration version

--- src/features/tasks/components/NewTaskPage.tsx
+++ src/features/tasks/components/NewTaskPage.tsx
@@ -1,67 +1,42 @@
-"use client"
-import { Suspense, useEffect, useRef, useState } from "react"
-import { Button } from "@/components/ui/button"
-import { useForm, FormProvider, FieldErrors } from "react-hook-form"
-import TaskPostingEditor from "./richTextEdito/Tiptap"
-import NewTaskSidebar from "./newTaskSidebar"
-import { useTask } from "@/contexts/TaskContext"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { TaskSchema, taskSchema } from "../server/task-types"
+"use client";
+import { Suspense, useEffect, useRef, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { useForm, FormProvider, FieldErrors } from "react-hook-form";
+import TaskPostingEditor from "./richTextEdito/Tiptap";
+import NewTaskSidebar from "./newTaskSidebar";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { TaskSchema, taskSchema } from "../server/task-types";
 import {
   autoSaveDraftTask,
   createTaksPaymentCheckoutSession,
-  validateStripeSession,
-} from "../server/action"
-import { toast } from "sonner"
-import { CircleAlert, Loader } from "lucide-react"
-import useCurrentUser from "@/hooks/useCurrentUser"
-import { UploadedFileMeta } from "@/features/media/server/media-types"
-import { useAutoSave } from "@/hooks/useAutoDraftSave"
-import { useAuthGate } from "@/hooks/useAuthGate"
-import Loading from "@/app/dashboard/solver/workspace/start/[taskId]/loading"
-import AuthGate from "@/components/AuthGate"
-import { useRouter, useSearchParams } from "next/navigation"
-import { useQuery } from "@tanstack/react-query"
-import { useFileUpload } from "@/hooks/useFile"
-import { env } from "@/env/client"
+} from "../server/action";
+import { toast } from "sonner";
+import { CircleAlert, Loader } from "lucide-react";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { UploadedFileMeta } from "@/features/media/server/media-types";
+import { useAutoSave } from "@/hooks/useAutoDraftSave";
+import { useAuthGate } from "@/hooks/useAuthGate";
+import Loading from "@/app/dashboard/solver/workspace/start/[taskId]/loading";
+import AuthGate from "@/components/AuthGate";
+import { useFileUpload } from "@/hooks/useFile";
+import { env } from "@/env/client";
+import useStripeSessionValidate from "../lib/useStripeSessionValidate";
+import { NewuseTask } from "@/contexts/TaskContext";
 
 export default function TaskCreationPage({
   defaultValues,
 }: {
-  defaultValues: TaskSchema
+  defaultValues: TaskSchema;
 }) {
-  const {
-    category,
-    deadline,
-    price,
-    visibility,
-    content,
-    selectedFiles,
-    description,
-    title,
-  } = useTask()
-  const currentUser = useCurrentUser()
-  const { user } = currentUser
-  const [isDisabled, setIsDisabled] = useState(true)
-  const { isLoading:authLoading, isBlocked } = useAuthGate()
-  const [isSheetOpen, setIsSheetOpen] = useState(false)
-  const searchParams = useSearchParams()
-  const hasShownToast = useRef(false)
-  const sessionId = searchParams.get("session_id")
-  const router = useRouter()
-
-  const { data: isValidSession, isLoading} = useQuery({
-    queryKey:['stripe-session', sessionId],
-    enabled:!!sessionId,
-    queryFn:() => validateStripeSession(sessionId!)
-  })
+  const { draft, selectedFiles } = NewuseTask(); //new Migrations
+  const { category, content, deadline, description, price, title, visibility } =
+    draft;
+  const currentUser = useCurrentUser();
+  const { user } = currentUser;
+  const [isDisabled, setIsDisabled] = useState(true);
+  const { isLoading: authLoading, isBlocked } = useAuthGate();
+  const [isSheetOpen, setIsSheetOpen] = useState(false);
   const { uploadMutate, isUploading } = useFileUpload();
- useEffect(()=>{
-    if (isValidSession && !hasShownToast.current) {
-    toast.success("Task published successfully!")
-    router.replace(window.location.pathname)
-    hasShownToast.current = true}
- },[isValidSession,router])
 
   useAutoSave({
     autoSaveFn: autoSaveDraftTask,
@@ -76,21 +51,20 @@ export default function TaskCreationPage({
       deadline,
     ],
     delay: 700,
-  })
+  });
 
   useEffect(() => {
-    if (typeof window === "undefined") return
-    const doc = new DOMParser().parseFromString(content, "text/html")
-    const text = doc.body.textContent?.trim() || ""
+    if (typeof window === "undefined") return;
+    const doc = new DOMParser().parseFromString(content, "text/html");
+    const text = doc.body.textContent?.trim() || "";
 
-    setIsDisabled(text.length < 5)
-  }, [content])
+    setIsDisabled(text.length < 5);
+  }, [content]);
 
-
-  const form = useForm<TaskSchema>({
+  const form = useForm({
     resolver: zodResolver(taskSchema),
     defaultValues,
-  })
+  });
 
   useEffect(() => {
     form.reset({
@@ -101,7 +75,7 @@ export default function TaskCreationPage({
       visibility,
       category,
       price,
-    })
+    });
   }, [
     form,
     title,
@@ -111,23 +85,26 @@ export default function TaskCreationPage({
     visibility,
     category,
     price,
-  ])
+  ]);
 
-  if (authLoading) return <Loading />
-  if (isBlocked) return <AuthGate />
+  if (authLoading) return <Loading />;
+  if (isBlocked) return <AuthGate />;
   async function onSubmit(data: TaskSchema) {
     try {
-      toast.loading("uploading files",{id:"file-upload"})
+      toast.loading("uploading files", { id: "file-upload" });
       let uploadedFileMetadata: UploadedFileMeta[] | undefined;
-      let uploadedFilesString :string | undefined
+      let uploadedFilesString: string | undefined;
       if (selectedFiles && selectedFiles.length > 0) {
-        uploadedFileMetadata = await uploadMutate({files:selectedFiles,scope:"task",url:`${env.NEXT_PUBLIC_GO_API_URL}/media`});//golang api
+        uploadedFileMetadata = await uploadMutate({
+          files: selectedFiles,
+          scope: "task",
+          url: `${env.NEXT_PUBLIC_GO_API_URL}/media`,
+        }); //golang api
         uploadedFilesString = JSON.stringify(uploadedFileMetadata);
       } else {
-      
         toast.dismiss("file-upload");
       }
-      await autoSaveDraftTask(
+      const lastUpdDraft = await autoSaveDraftTask(
         title,
         description,
         content,
@@ -137,27 +114,26 @@ export default function TaskCreationPage({
         visibility,
         deadline,
         uploadedFilesString
-        
-      )
-       toast.dismiss("file-upload");
-      await createTaksPaymentCheckoutSession(
-        {price,
-        userId:user?.id!,
-        deadlineStr:deadline}
-      )
-     
+      );
+      toast.dismiss("file-upload");
+      await createTaksPaymentCheckoutSession({
+        price,
+        userId: user?.id!,
+        deadlineStr: deadline,
+        draftTaskId: lastUpdDraft?.id!,
+      });
     } catch (e) {
-      console.error(e)
-      toast.error(`${(<CircleAlert />)}something Went Wrong`)
+      console.error(e);
+      toast.error(`${(<CircleAlert />)}something Went Wrong`);
     }
   }
   function onError(errors: FieldErrors<TaskSchema>) {
-    console.warn("Validation errors ❌", errors)
-    setIsSheetOpen(true)
+    console.warn("Validation errors ❌", errors);
+    setIsSheetOpen(true);
 
-    const firstErrorField = Object.keys(errors)[0]
-    const el = document.querySelector(`[name="${firstErrorField}"]`)
-    if (el) el.scrollIntoView({ behavior: "smooth", block: "center" })
+    const firstErrorField = Object.keys(errors)[0];
+    const el = document.querySelector(`[name="${firstErrorField}"]`);
+    if (el) el.scrollIntoView({ behavior: "smooth", block: "center" });
   }
   return (
     <div className="flex h-full bg-background ">
@@ -202,5 +178,5 @@ export default function TaskCreationPage({
         </FormProvider>
       </div>
     </div>
-  )
+  );
 }
