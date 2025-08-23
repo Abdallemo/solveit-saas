88d5afd48b0a00d855f7bffc7afc82ec0e7663fe
EngCamo <learn3038it@gmail.com>
2025-08-24 02:51:30 +0800

    authorization utility update to accept an array of authorized roles

--- src/app/dashboard/solver/workspace/[workspaceId]/page.tsx
+++ src/app/dashboard/solver/workspace/[workspaceId]/page.tsx
@@ -1,7 +1,7 @@
 import WorkspacePageComp from "@/features/tasks/components/WorkspacePageComp";
 import { isAuthorized } from "@/features/auth/server/actions";
 export default async function Page() {
-  await isAuthorized("SOLVER");
+  await isAuthorized(["SOLVER"]);
 
   return <WorkspacePageComp />;
 }
