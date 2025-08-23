da3616e838bf085040c0988e0333dbd8291da3a0
EngCamo <learn3038it@gmail.com>
2025-08-24 02:49:46 +0800

    authorization utility update to accept an array of authorized roles

--- src/app/dashboard/admin/layout.tsx
+++ src/app/dashboard/admin/layout.tsx
@@ -2,7 +2,7 @@ import { isAuthorized } from "@/features/auth/server/actions";
 import { ReactNode } from "react";
 
 export default async function Layout({ children }: { children: ReactNode }) {
-  await isAuthorized("ADMIN");
+  await isAuthorized(["ADMIN"]);
 
   return <>{children}</>;
 }
