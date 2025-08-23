5d4a38e02b7e971c3cf6265635f0ba519dd2b47b
EngCamo <learn3038it@gmail.com>
2025-08-24 03:51:49 +0800

    increased the query limit to 8

--- src/app/dashboard/tasks/page.tsx
+++ src/app/dashboard/tasks/page.tsx
@@ -16,7 +16,7 @@ export default async function ServerWrapper({
   const { q, page } = await searchParams
   const search = q ?? ""
   const pages = Number.parseInt(page ?? "1")
-  const limit = 6
+  const limit = 8
   const offset = (pages - 1) * limit
 
   const { tasks, totalCount } = await getAllTasksByRolePaginated(currentUser.id,currentUser.role!, {
