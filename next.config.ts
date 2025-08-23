7ff19a95ac90071c5cf86e6b47de674e19720552
EngCamo <learn3038it@gmail.com>
2025-08-24 01:48:30 +0800

    nextjs configs to allow google images

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,24 @@
 import type { NextConfig } from "next";
-import withBundleAnalyzer from '@next/bundle-analyzer';
+import withBundleAnalyzer from "@next/bundle-analyzer";
 const withAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
+  enabled: process.env.ANALYZE === "true",
 });
 
 const nextConfig: NextConfig = {
-  experimental:{
-    
-    serverActions:{
-      bodySizeLimit:'100mb'
-    }
-  }
+  images: {
+    remotePatterns: [
+      {
+        hostname: "lh3.googleusercontent.com",
+        pathname:"**"
+
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "100mb",
+    },
+  },
 };
 
 export default withAnalyzer(nextConfig);
