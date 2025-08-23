fff7db08ca270bce81f8192423656fa7d3e5544b
EngCamo <learn3038it@gmail.com>
2025-08-24 01:49:01 +0800

    added font dep

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,20 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GeistMono } from "geist/font/mono";
+import { GeistSans, } from "geist/font/sans";
 import { SessionProvider, useSession } from "next-auth/react";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "SolveIt",
@@ -30,7 +32,7 @@ export default function RootLayout({
     <SessionProvider >
       <html lang="en" suppressHydrationWarning >
         <body
-          className={`${geistSans.variable} ${geistMono.variable}  antialiased  `}>
+          className={`${GeistSans.variable} ${GeistMono.variable} font-sans antialiased`}>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
