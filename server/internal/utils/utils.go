2c522269f9e86400bce24365e0a7cf45c8c876dc
EngCamo <learn3038it@gmail.com>
2025-08-25 05:11:16 +0800

    added a cache key maker using sha256

--- server/internal/utils/utils.go
+++ server/internal/utils/utils.go
@@ -6,10 +6,7 @@ import (
 )
 
 func MakeCacheKey(prefix, content string) string {
-	// hash the content
 	hash := sha256.Sum256([]byte(content))
-	// convert to hex string
 	hashStr := hex.EncodeToString(hash[:])
-	// return prefix + hash
 	return prefix + hashStr
 }
