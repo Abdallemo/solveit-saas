b29bf0e1bd71e1f82402c6de6b4b4be2bfc171fc
EngCamo <learn3038it@gmail.com>
2025-08-25 05:08:07 +0800

    added redis instacne

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -14,12 +14,15 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/go-redis/redis/v8"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 	"github.com/sashabaranov/go-openai"
 )
 
 func main() {
+	ctx := context.Background()
+
 	log.Println("Go version:", runtime.Version())
 	_, b, _, _ := runtime.Caller(0)
 	basepath := filepath.Join(filepath.Dir(b), "..", "..")
@@ -27,7 +30,7 @@ func main() {
 	if err := godotenv.Load(dotenvPath); err != nil {
 		log.Println("No .env file found, falling back to system env")
 	}
-	cfg, err := config.LoadDefaultConfig(context.Background(),
+	cfg, err := config.LoadDefaultConfig(ctx,
 		config.WithRegion("auto"),
 		config.WithCredentialsProvider(
 			credentials.NewStaticCredentialsProvider(
@@ -41,10 +44,20 @@ func main() {
 		log.Fatalf("failed to load config: %v", err)
 	}
 	dbUrl := os.Getenv("DATABASE_URL")
-	db, err := pgxpool.New(context.Background(), dbUrl)
+	db, err := pgxpool.New(ctx, dbUrl)
 	if err != nil {
 		log.Fatal("unable to connect to database:", err)
 	}
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_URL"),
+		Password: os.Getenv("REDIS_PASS"),
+		DB:       0,
+	})
+	_, err = redisClient.Ping(ctx).Result()
+	if err != nil {
+		log.Fatal("unable to connect to redis instance")
+	}
+
 	defer db.Close()
 	store := storage.NewSPostgressStore(db)
 
@@ -52,6 +65,6 @@ func main() {
 		o.BaseEndpoint = aws.String(os.Getenv("S3_ENDPOINT"))
 	})
 	openaiClient := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
-	server := api.NewServer(":3030", s3Client, openaiClient, store)
+	server := api.NewServer(":3030", s3Client, openaiClient, store, redisClient)
 	log.Fatal(server.Run())
 }
