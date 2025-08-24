52f4a2a76d16f862dc4841b73647880e5b513279
EngCamo <learn3038it@gmail.com>
2025-08-25 05:12:28 +0800

    fixed catgroy z-index issue on mobile

--- src/features/tasks/components/CategorySelectWrapper.tsx
+++ src/features/tasks/components/CategorySelectWrapper.tsx
@@ -18,44 +18,68 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import {getAllTaskCatagories } from "../server/action";
+import { getAllTaskCatagories } from "../server/action";
 import { NewuseTask } from "@/contexts/TaskContext";
 import { useQuery } from "@tanstack/react-query";
+import { useIsMobile } from "@/hooks/use-mobile";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 interface CategoryCompsProps {
   value: string;
   onChange: (value: string) => void;
   name?: string;
+  isPending: boolean;
 }
 
-export function CategoryComps({ value, onChange }: CategoryCompsProps) {
+export function CategoryComps({
+  value,
+  onChange,
+  isPending: isLoading,
+}: CategoryCompsProps) {
   const [open, setOpen] = useState(false);
   // const { setCategory } = useTask(); // migrated from
   const { updateDraft } = NewuseTask(); //Migrated to
+  const isMobile = useIsMobile();
 
-  const {data:fetchedCategories,isPending} = useQuery({
+  const { data: fetchedCategories, isPending } = useQuery({
     queryKey: ["category"],
     queryFn: async () => await getAllTaskCatagories(),
   });
-  // useEffect(() => {
-  
-  //   const fetchCategories = async () => {
-  //     try {
-  //       const fetched = await getAllTaskCatagories();
-  //       setCategories(fetched);
-  //     } catch (err) {
-  //       console.error("Failed to fetch categories:", err);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-  //   fetchCategories();
-  // }, []);
-
+  if (isMobile)
+    return (
+      <Select
+        disabled={isPending || isLoading}
+        value={value}
+        onValueChange={(v) => {
+          onChange(v);
+          updateDraft({ category: v });
+        }}>
+        <SelectTrigger className="w-full">
+          <SelectValue placeholder="Select category" />
+        </SelectTrigger>
+        <SelectContent className="w-full">
+          <SelectGroup>
+            {fetchedCategories?.map((cat) => (
+              <SelectItem key={cat.id} value={cat.name}>
+                {cat.name}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    );
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
         <Button
+          disabled={isPending || isLoading}
           variant="outline"
           role="combobox"
           className="w-full justify-between">
@@ -65,13 +89,13 @@ export function CategoryComps({ value, onChange }: CategoryCompsProps) {
           <ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-fit p-0">
+      <PopoverContent className="w-fit p-0 z">
         <Command>
           <CommandInput placeholder="Search Category..." />
           <CommandList>
             <CommandEmpty>No Category found.</CommandEmpty>
             <CommandGroup>
-              {isPending && <Loader2 className="animate-spin"/>}
+              {isPending && <Loader2 className="animate-spin" />}
               {fetchedCategories?.map((cat) => (
                 <CommandItem
                   key={cat.id}
