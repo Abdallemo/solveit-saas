8ca34816164b66b8ef907b75082007eeb611480e
EngCamo <learn3038it@gmail.com>
2025-08-26 02:30:55 +0800

    some changes

--- src/app/dashboard/solver/workspace/[workspaceId]/layout.tsx
+++ src/app/dashboard/solver/workspace/[workspaceId]/layout.tsx
@@ -1,5 +1,8 @@
 import { WorkspaceProvider } from "@/contexts/WorkspaceContext";
-import { getServerUserSession, isAuthorized } from "@/features/auth/server/actions";
+import {
+  getServerUserSession,
+  isAuthorized,
+} from "@/features/auth/server/actions";
 import { getWorkspaceById } from "@/features/tasks/server/action";
 import { redirect } from "next/navigation";
 
@@ -12,20 +15,21 @@ export default async function WorkspaceLayout({
   children: ReactNode;
   params: Promise<{ workspaceId: string }>;
 }) {
-  
-  const {user} = await isAuthorized(["SOLVER"]);
+  const { user } = await isAuthorized(["SOLVER"]);
   const { workspaceId } = await params;
-  const currentWorkspace = await getWorkspaceById(workspaceId,user?.id!);
+  const currentWorkspace = await getWorkspaceById(workspaceId, user?.id!);
   if (!currentWorkspace) {
     throw new Error("Workspace not found");
   }
-  if ( currentWorkspace.solverId !== user?.id){
-    redirect("/dashboard/"); 
-    
+  if (currentWorkspace.solverId !== user?.id) {
+    redirect("/dashboard/");
   }
-  
+  const serverCurrentTime = new Date().toISOString();
+
   return (
-    <WorkspaceProvider workspace={currentWorkspace}>
+    <WorkspaceProvider
+      workspace={currentWorkspace}
+      serverCurrentTime={serverCurrentTime}>
       {children}
     </WorkspaceProvider>
   );
