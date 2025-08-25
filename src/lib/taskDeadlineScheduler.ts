d94385681540cf06d0d174b51a61a245dce266c1
EngCamo <learn3038it@gmail.com>
2025-08-26 02:31:45 +0800

    some changes

--- src/lib/taskDeadlineScheduler.ts
+++ src/lib/taskDeadlineScheduler.ts
@@ -4,18 +4,18 @@ import { logger } from "./logging/winston";
 
 const INTERVAL = 10 * 60 * 1000; 
 
-export function startDeadlineScheduler() {
-  setInterval(async () => {
-    try {
-      const tasks = await getAllTasks(); 
+// export function startDeadlineScheduler() {
+//   setInterval(async () => {
+//     try {
+//       const tasks = await getAllTasks(); 
 
-      for (const task of tasks) {
-       await handleTaskDeadline(task)
-      }
+//       for (const task of tasks) {
+//        await handleTaskDeadline(task)
+//       }
 
-      logger.info("[DeadlineScheduler] Checked all tasks.");
-    } catch (error) {
-      logger.error("DeadlineScheduler error:", {error:error});
-    }
-  }, INTERVAL);
-}
+//       logger.info("[DeadlineScheduler] Checked all tasks.");
+//     } catch (error) {
+//       logger.error("DeadlineScheduler error:", {error:error});
+//     }
+//   }, INTERVAL);
+// }
