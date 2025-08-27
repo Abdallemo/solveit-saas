046cf67148262b808341288fa806d874f8ae7732
EngCamo <learn3038it@gmail.com>
2025-08-27 09:06:37 +0800

    minro changes

---
+++ src/app/dashboard/poster/(mentorship)/mentors/page.tsx
@@ -0,0 +1,13 @@
+import { MentorsBrowser } from "@/features/mentore/components/BrowseMentor"
+import {  getMentorListigWithAvailbelDates, MentorListType } from "@/features/mentore/server/action"
+import { logger } from "@/lib/logging/winston"
+
+export  default async function MentorsPage() {
+  const mentorWithj = await getMentorListigWithAvailbelDates()
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <MentorsBrowser mentors={mentorWithj}  />
+    </div>
+  )
+}
