231c355b9ecf4cc1f7f5791d3f64b27c4b118e65
EngCamo <learn3038it@gmail.com>
2025-08-27 09:10:14 +0800

    comprehnsive changes of mentor action added payments booking queries and solved booking slots show after booked

--- src/features/mentore/server/action.ts
+++ src/features/mentore/server/action.ts
@@ -1,12 +1,31 @@
 "use server";
 
 import db from "@/drizzle/db";
-import { MentorshipProfileTable } from "@/drizzle/schemas";
-import { getServerUserSession } from "@/features/auth/server/actions";
-import { getServerUserSubscriptionById } from "@/features/users/server/actions";
+import {
+  MentorshipBookingTable,
+  MentorshipProfileTable,
+  MentorshipSessionTable,
+  PaymentPorposeEnumType,
+} from "@/drizzle/schemas";
+import {
+  getServerUserSession,
+  isAuthorized,
+} from "@/features/auth/server/actions";
+import {
+  getServerUserSubscriptionById,
+  getUserById,
+  UpdateUserField,
+} from "@/features/users/server/actions";
 import { MentorError, SubscriptionError } from "@/lib/Errors";
 import { logger } from "@/lib/logging/winston";
-import { eq } from "drizzle-orm";
+import { and, eq, gte } from "drizzle-orm";
+import { addDays, startOfWeek, isFuture, format, set } from "date-fns";
+import { calculateSlotDuration, daysInWeek } from "@/lib/utils";
+import { BookingFormData, bookingSchema } from "./types";
+import { getServerReturnUrl } from "@/features/subscriptions/server/action";
+import { stripe } from "@/lib/stripe";
+import { env } from "@/env/server";
+import { redirect } from "next/navigation";
 
 export type AvailabilitySlot = {
   day: string;
@@ -35,20 +54,22 @@ export async function validateMentorAccess() {
 
   return { user, usrSub };
 }
-export type MentorListType = Exclude<Awaited<ReturnType<typeof getMentorListig>>, any[]> & { availableTimes: AvailabilitySlot[] };
+export type MentorListType = Exclude<
+  Awaited<ReturnType<typeof getMentorListigProfile>>,
+  any[]
+>;
+export type MentorListigWithAvailbelDates = Awaited<
+  ReturnType<typeof getMentorListigWithAvailbelDates>
+>[number];
 
-export async function getMentorListig(t: "poster" | "solver++") {
+export async function getMentorListigProfile() {
   const { user, usrSub } = await validateMentorAccess();
-  if (t === "solver++") {
-    const result = await db.query.MentorshipProfileTable.findFirst({
-      where: (tb, fn) => fn.eq(tb.userId, user.id!),
-    });
-    return {...result!,displayName:result?.displayName || user.name!};
-  } else {
-    const results = await db.query.MentorshipProfileTable.findMany();
-    return results;
-  }
+  const result = await db.query.MentorshipProfileTable.findFirst({
+    where: (tb, fn) => fn.eq(tb.userId, user.id!),
+  });
+  return { ...result!, displayName: result?.displayName || user.name! };
 }
+
 export async function handleProfilePublishState(isPublished: boolean) {
   const { user, usrSub } = await validateMentorAccess();
   await db
@@ -83,7 +104,14 @@ export async function saveMentorListing(values: {
   availableTimes: AvailabilitySlot[];
   ratePerHour: number;
 }) {
-  const { displayName,title, description, availableTimes, ratePerHour, avatar } = values;
+  const {
+    displayName,
+    title,
+    description,
+    availableTimes,
+    ratePerHour,
+    avatar,
+  } = values;
   try {
     const { user, usrSub } = await validateMentorAccess();
 
@@ -95,7 +123,7 @@ export async function saveMentorListing(values: {
         displayName,
         description,
         avatar,
-        availableTimes: JSON.stringify(availableTimes),
+        availableTimes: availableTimes,
         ratePerHour,
       })
       .onConflictDoUpdate({
@@ -104,7 +132,7 @@ export async function saveMentorListing(values: {
           displayName,
           title,
           description,
-          availableTimes: JSON.stringify(availableTimes),
+          availableTimes: availableTimes,
           ratePerHour,
         },
       });
@@ -115,5 +143,199 @@ export async function saveMentorListing(values: {
       stack: (error as Error).stack,
     });
     throw error;
+  }
+}
+
+function toYMD(date: Date): string {
+  return format(date, "yyyy-MM-dd"); 
+}
+
+const getSessionKey = (dateYMD: string, slot: AvailabilitySlot) =>
+  `${dateYMD}-${slot.day}-${slot.start}-${slot.end}`;
+export async function getMentorListigWithAvailbelDates() {
+  const allMentors = await db.query.MentorshipProfileTable.findMany();
+
+  const allBookedSessions = await db.query.MentorshipSessionTable.findMany({
+    with: {
+      bookedSessions: {
+        with: {
+          solver: { columns: { id: true } },
+        },
+      },
+    },
+  });
+  const bookedKeys = new Set(
+    allBookedSessions.map((session) => {
+      const solverId = session.bookedSessions.solverId
+      return `${solverId}-${getSessionKey(
+        session.sessionDate,
+        session.timeSlot
+      )}`;
+    })
+  );
+
+  const mentorsWithFilteredAvailability = allMentors.map((mentor) => {
+    const availableTimes = mentor.availableTimes as AvailabilitySlot[];
+    const availableDates: { date: Date; slot: AvailabilitySlot }[] = [];
+
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    const weekStart = startOfWeek(today, { weekStartsOn: 0 });
+
+    for (const slot of availableTimes) {
+      const dayIndex = daysInWeek.indexOf(slot.day.toLowerCase());
+      if (dayIndex === -1) continue;
+
+      for (let week = 0; week < 12; week++) {
+        const dateObj = addDays(weekStart, week * 7 + dayIndex);
+        if (!isFuture(dateObj)) continue;
+
+        const ymd = toYMD(dateObj); 
+        const key = `${mentor.userId}-${getSessionKey(ymd, slot)}`;
+
+        if (!bookedKeys.has(key)) {
+          availableDates.push({ date: dateObj, slot }); 
+        }
+      }
+    }
+
+    return { ...mentor, availableDates };
+  });
+
+  return mentorsWithFilteredAvailability;
+}
+
+export async function createMentorBookingPaymentCheckout(values: {
+  data: BookingFormData;
+  mentor: MentorListigWithAvailbelDates;
+}) {
+  const referer = await getServerReturnUrl();
+  try {
+    const { user: currentUser, authorized } = await isAuthorized(["POSTER"]);
+    const { data, mentor } = values;
+    if (!authorized || !currentUser?.id) throw new Error("unAuthorized");
+    const validData = bookingSchema.safeParse(data);
+    const userId = currentUser.id;
+    if (!validData.success) throw new Error("all fields are requred");
+    if (validData.data.sessions.length === 0) return;
+
+    const totalPrice = validData.data.sessions.reduce((sum, session) => {
+      const duration = calculateSlotDuration(session.slot);
+      return sum + mentor.ratePerHour * duration;
+    }, 0);
+
+    const user = await getUserById(userId);
+
+    if (!user || !user.id) return;
+    let customerId = user.stripeCustomerId;
+    if (!user.stripeCustomerId) {
+      const newCustomer = await stripe.customers.create({
+        email: currentUser.email!,
+        name: user.name ?? "",
+        metadata: {
+          userId: userId,
+        },
+      });
+      customerId = newCustomer.id;
+      await UpdateUserField({
+        id: user.id,
+        data: { stripeCustomerId: customerId },
+      });
+    }
+    let bookingId = "";
+    await db.transaction(async (tx) => {
+      const result = await tx
+        .insert(MentorshipBookingTable)
+        .values({
+          posterId: userId,
+          solverId: mentor.userId,
+          price: totalPrice,
+          notes: data.notes,
+          status: "PENDING",
+        })
+        .returning({ bookingId: MentorshipBookingTable.id });
+      bookingId = result[0].bookingId;
+      logger.warn(
+        "when assigned from the db transaction bookingId: " + bookingId
+      );
+
+      await Promise.all(
+        data.sessions.map((session) =>
+          tx.insert(MentorshipSessionTable).values({
+            bookingId: bookingId,
+            sessionDate: format(session.date, "yyyy-MM-dd"),
+            timeSlot: session.slot,
+          })
+        )
+      );
+    });
+    logger.warn("before creating checkout sessoin bookingId: " + bookingId);
+    const session = await stripe.checkout.sessions.create({
+      mode: "payment",
+      customer: customerId!,
+
+      line_items: [
+        {
+          price_data: {
+            currency: "myr",
+            product_data: {
+              name: "Mentor Booking" as PaymentPorposeEnumType,
+            },
+            unit_amount: totalPrice * 100,
+          },
+
+          quantity: 1,
+        },
+      ],
+
+      payment_method_types: ["card"],
+      payment_method_options: {
+        card: {
+          setup_future_usage: "off_session",
+        },
+      },
+      payment_intent_data: {
+        setup_future_usage: "off_session",
+      },
+
+      metadata: {
+        userId,
+        type: "Mentor Booking" as PaymentPorposeEnumType,
+        bookingId,
+      },
+      cancel_url: `${referer}?booking_id=${bookingId}`,
+      success_url: `${env.NEXTAUTH_URL}/dashboard/poster/bookings/?session_id={CHECKOUT_SESSION_ID}&booking_id=${bookingId}`,
+      saved_payment_method_options: {
+        allow_redisplay_filters: ["always", "limited", "unspecified"],
+        payment_method_save: "enabled",
+      },
+    });
+
+    if (!session.url) throw new Error("Failed to create checkout session");
+    redirect(session.url);
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function updateMentorBooking(
+  bookingId: string,
+  paymentId: string
+) {
+  try {
+    const result = await db
+      .update(MentorshipBookingTable)
+      .set({
+        status: "PAID",
+        paymentId,
+      })
+      .where(eq(MentorshipBookingTable.id, bookingId))
+      .returning({ bookingId: MentorshipBookingTable.id });
+    return result;
+  } catch (error) {
+    logger.error(
+      "failed to update Mentor Temperory Booking\n" + (error as Error).message
+    );
+    throw new Error("failed to update Mentor Temperory Booking");
   }
 }
