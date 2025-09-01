048496ddd38c2b41b4182e2afa7648d85c1fe923
EngCamo <learn3038it@gmail.com>
2025-09-01 11:26:56 +0800

    changes

--- src/features/mentore/server/types.ts
+++ src/features/mentore/server/types.ts
@@ -23,5 +23,5 @@ export const mentorListingSchema = z.object({
   displayName:z.string().min(5,{message:"names should be at least 5 charecter long"}),
   title:z.string().min(5,{message:"title should be at least 5 charecter long"}),
   description:z.string().min(5,{message:"description should be at least 5 charecter long"}),
-  ratePerHour:z.coerce.number().min(1,{message:"hourly rate should be at least RM1"})
+  ratePerHour:z.coerce.number().min(2,{message:"hourly rate should be at least RM2"})
 })
