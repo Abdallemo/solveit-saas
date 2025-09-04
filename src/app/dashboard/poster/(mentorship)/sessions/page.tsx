2ab9524115bb53cc88cddb18bab0cd6242a02788
EngCamo <learn3038it@gmail.com>
2025-09-04 18:53:35 +0800

    changes

--- src/app/dashboard/poster/(mentorship)/sessions/page.tsx
+++ src/app/dashboard/poster/(mentorship)/sessions/page.tsx
@@ -1,5 +1,7 @@
-export default function page() {
-  return (
-    <div>bookings</div>
-  )
+import { SessionsList } from "@/features/mentore/components/MentroSessionPageCompx";
+import { getMentorBookingSessions } from "@/features/mentore/server/action";
+
+export default async function Page() {
+  const bookings = await getMentorBookingSessions();
+  return <SessionsList booking={bookings} />;
 }
