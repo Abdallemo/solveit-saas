1960a205353a1f54b876ed7e8a5db4a3bb1ac0bb
EngCamo <learn3038it@gmail.com>
2025-09-04 18:53:51 +0800

    changes

--- src/app/dashboard/solver/(mentorship)/mentor/page.tsx
+++ src/app/dashboard/solver/(mentorship)/mentor/page.tsx
@@ -1,5 +1,13 @@
-export default function MentorSessionPage() {
+import { SessionsList } from "@/features/mentore/components/MentroSessionPageCompx";
+import { getMentorBookingSessions } from "@/features/mentore/server/action";
+
+export default async function Page() {
+  const bookings = await getMentorBookingSessions();
+  console.log((bookings));
   return (
-    <div>MentorSessionPage</div>
-  )
+    <div className="w-full h-full mx-auto p-10   ">
+      <h1 className="text-2xl font-semibold mb-6">Your Sessions</h1>
+      <SessionsList booking={bookings} />
+    </div>
+  );
 }
