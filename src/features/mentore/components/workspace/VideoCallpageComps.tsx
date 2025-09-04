abcf2fdbe8e983a078df43581cd44fbb27dac86a
EngCamo <learn3038it@gmail.com>
2025-09-04 18:54:32 +0800

    changes

---
+++ src/features/mentore/components/workspace/VideoCallpageComps.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Video } from "lucide-react";
+
+export function VideoCallPageComps() {
+  return (
+    <div className="flex flex-col w-full h-full justify-center items-center overflow-hidden rounded-lg border border-white/5">
+      <Video />
+    </div>
+  )
+}
