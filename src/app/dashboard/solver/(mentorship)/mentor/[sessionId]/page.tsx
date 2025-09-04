f4310efae532773bb1726f437671da806890b6eb
EngCamo <learn3038it@gmail.com>
2025-09-04 18:53:55 +0800

    changes

---
+++ src/app/dashboard/solver/(mentorship)/mentor/[sessionId]/page.tsx
@@ -0,0 +1,14 @@
+import MentorshipWorkspace from "@/features/mentore/components/workspace/MentorshipWorkspace";
+import { getMentorSession } from "@/features/mentore/server/action";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ sessionId: string }>;
+}) {
+  const { sessionId } = await params;
+  const session = await getMentorSession(sessionId)
+  if (!session)return
+
+  return <MentorshipWorkspace mentorWorkspace={session}/>;
+}
