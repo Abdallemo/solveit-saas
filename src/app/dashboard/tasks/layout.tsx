4e95d8bb09a8b740945b745d9c13697a0b44a4bf
EngCamo <learn3038it@gmail.com>
2025-09-06 05:18:27 +0800

    completed chating feature

--- src/app/dashboard/tasks/layout.tsx
+++ src/app/dashboard/tasks/layout.tsx
@@ -1,24 +1,24 @@
 import {
   getServerUserSession,
+  isAuthorized,
 } from "@/features/auth/server/actions";
 import { ReactNode } from "react";
 import { getUserById } from "@/features/users/server/actions";
 import { redirect } from "next/navigation";
 
 export default async function Layout({ children }: { children: ReactNode }) {
-  const currentUser = await getServerUserSession();
+  const { user } = await isAuthorized(["POSTER", "SOLVER"]);
 
-  if (!currentUser) {
+  if (!user || !user.id) {
     return redirect("/api/auth/signout?callbackUrl=/login");
   }
 
-  const userInDb = await getUserById(currentUser.id!);
+  const userInDb = await getUserById(user.id);
   if (!userInDb) {
     return redirect(
       "/api/auth/signout?callbackUrl=/login?error=account_deleted"
     );
   }
-
 
   return <>{children}</>;
 }
