385ef6da4ebf9b97db87e4652e6cd7fcb2072d7a
EngCamo <learn3038it@gmail.com>
2025-09-05 12:22:54 +0800

    chat edtion

--- src/app/dashboard/solver/(mentorship)/mentor/page.tsx
+++ src/app/dashboard/solver/(mentorship)/mentor/page.tsx
@@ -3,7 +3,6 @@ import { getMentorBookingSessions } from "@/features/mentore/server/action";
 
 export default async function Page() {
   const bookings = await getMentorBookingSessions();
-  console.log((bookings));
   return (
     <div className="w-full h-full mx-auto p-10   ">
       <h1 className="text-2xl font-semibold mb-6">Your Sessions</h1>
