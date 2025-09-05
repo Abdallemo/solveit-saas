05d733b25f6c51e5b63dccb61a342b341a8946ad
EngCamo <learn3038it@gmail.com>
2025-09-05 17:10:14 +0800

    chat feature completed

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
 import withBundleAnalyzer from "@next/bundle-analyzer";
+
 const withAnalyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
 });
@@ -8,9 +9,14 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "https",
         hostname: "lh3.googleusercontent.com",
-        pathname:"**"
-
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "pub-c60addcb244c4d23b18a98d686f3195e.r2.dev",
+        pathname: "/**",
       },
     ],
   },
