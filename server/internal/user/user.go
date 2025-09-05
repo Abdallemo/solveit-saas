6a18e69c92652d9c5863147651e468cb472a671d
EngCamo <learn3038it@gmail.com>
2025-09-06 05:18:25 +0800

    completed chating feature

---
+++ server/internal/user/user.go
@@ -0,0 +1,11 @@
+package user
+
+type PublicUser struct {
+	ID            string `json:"id"`
+	Name          string `json:"name"`
+	Role          string `json:"role"`
+	Image         string `json:"image"`
+	Email         string `json:"email"`
+	EmailVerified string `json:"emailVerified"`
+	CreatedAt     string `json:"createdAt"`
+}
