088434b9f9df6f3837f294e8d2899523588c67f5
EngCamo <learn3038it@gmail.com>
2025-09-06 10:37:42 +0800

    added interactive dashboards

---
+++ src/app/dashboard/poster/tasks/layout.tsx
@@ -0,0 +1,23 @@
+import {
+  isAuthorized,
+} from "@/features/auth/server/actions";
+import { ReactNode } from "react";
+import { getUserById } from "@/features/users/server/actions";
+import { redirect } from "next/navigation";
+
+export default async function Layout({ children }: { children: ReactNode }) {
+  const { user } = await isAuthorized(["POSTER"]);
+
+  if (!user || !user.id) {
+    return redirect("/api/auth/signout?callbackUrl=/login");
+  }
+
+  const userInDb = await getUserById(user.id);
+  if (!userInDb) {
+    return redirect(
+      "/api/auth/signout?callbackUrl=/login?error=account_deleted"
+    );
+  }
+
+  return <>{children}</>;
+}
