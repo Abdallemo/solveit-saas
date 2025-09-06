ce6bd9eb8c969973a8b489ef1b2fa20788cec922
EngCamo <learn3038it@gmail.com>
2025-09-06 10:37:40 +0800

    added interactive dashboards

--- src/app/dashboard/poster/page.tsx
+++ src/app/dashboard/poster/page.tsx
@@ -1,35 +1,5 @@
-import Pricing from "@/components/marketing/pricing";
-import {
-  getServerUserSession,
-  isAuthorized,
-} from "@/features/auth/server/actions";
-import {
-  getServerUserSubscriptionById,
-  getUserById,
-} from "@/features/users/server/actions";
-import { redirect } from "next/navigation";
+import PosterDashboard from "@/features/users/components/poster/PosterDashboard";
 
 export default async function page() {
-  const currentUser = await getServerUserSession();
-
-  if (!currentUser) {
-    return redirect("/api/auth/signout?callbackUrl=/login");
-  }
-
-  const userInDb = await getUserById(currentUser.id!);
-  if (!userInDb) {
-    return redirect(
-      "/api/auth/signout?callbackUrl=/login?error=account_deleted"
-    );
-  }
-  await isAuthorized(["POSTER"]);
-  const userRole = await getServerUserSubscriptionById(currentUser?.id);
-  const cache = Math.random() * 1000;
-  return (
-    <main className="flex flex-col w-full h-screen justify-center items-center">
-      <p>Dashbard</p>
-      <p>test cache :{cache}</p>
-
-    </main>
-  );
+  return <PosterDashboard />;
 }
