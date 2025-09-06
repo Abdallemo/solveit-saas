da8205f1ee5b0f0fb8f91382bc2e1e90388ffea4
EngCamo <learn3038it@gmail.com>
2025-09-06 10:37:49 +0800

    added interactive dashboards

---
+++ src/app/dashboard/poster/tasks/page.tsx
@@ -0,0 +1,50 @@
+import { getServerUserSession } from "@/features/auth/server/actions";
+import {
+  getAllCategoryMap,
+  getAllTasksByRolePaginated,
+} from "@/features/tasks/server/action";
+
+import DisplayListComponent from "@/features/tasks/components/DisplayComponent";
+
+export default async function ServerWrapper({
+  searchParams,
+}: {
+  searchParams: Promise<{ q: string; page: string }>;
+}) {
+  const currentUser = await getServerUserSession();
+  if (!currentUser || !currentUser.role || !currentUser.id) return;
+
+  const categoryMap = await getAllCategoryMap();
+  const { q, page } = await searchParams;
+  const search = q ?? "";
+  const pages = Number.parseInt(page ?? "1");
+  const limit = 8;
+  const offset = (pages - 1) * limit;
+
+  const { tasks, totalCount } = await getAllTasksByRolePaginated(
+    currentUser.id,
+    currentUser.role!,
+    {
+      search,
+      limit,
+      offset,
+    }
+  );
+
+  const totalPages = Math.ceil(totalCount / limit);
+  const hasPrevious = pages > 1;
+  const hasNext = pages < totalPages;
+  return (
+    <DisplayListComponent
+      title={"Public Tasks"}
+      itretable={tasks}
+      totalCount={totalCount}
+      categoryMap={categoryMap}
+      pages={pages}
+      totalPages={totalPages}
+      hasPrevious={hasPrevious}
+      hasNext={hasNext}
+      filterType="category"
+    />
+  );
+}
