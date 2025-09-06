38e29a4ce3f57eac4d40b2b1f108e11695c1ec9a
EngCamo <learn3038it@gmail.com>
2025-09-06 10:37:21 +0800

    added interactive dashboards

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,13 +1,12 @@
 import { isAuthorized } from "@/features/auth/server/actions";
-import AdminDashboardComponent from "@/features/users/components/admin/AdminHomePageComponent";
+import AdminDashboard from "@/features/users/components/admin/AdminHomePageComponent";
 import { getSummaryStats } from "@/features/users/server/actions";
 import { getLogs } from "@/lib/logging/action";
-export type StastType = Awaited<ReturnType<typeof getSummaryStats>>
+export type StastType = Awaited<ReturnType<typeof getSummaryStats>>;
 export default async function page() {
   await isAuthorized(["ADMIN"]);
   const serverLogs = await getLogs();
   const stats = await getSummaryStats();
-  
-  
-  return <AdminDashboardComponent serverLogs={serverLogs} stats ={stats} />;
+
+  return <AdminDashboard serverLogs={serverLogs} />;
 }
