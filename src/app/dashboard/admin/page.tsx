6a62b369bdaf9ed1dca329f19b89b3dd718018e6
EngCamo <learn3038it@gmail.com>
2025-09-06 14:34:12 +0800

    added admin analytic dashboard

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,12 +1,11 @@
-import { isAuthorized } from "@/features/auth/server/actions";
+import { getAdminStats } from "@/features/tasks/server/action";
 import AdminDashboard from "@/features/users/components/admin/AdminHomePageComponent";
 import { getSummaryStats } from "@/features/users/server/actions";
 import { getLogs } from "@/lib/logging/action";
 export type StastType = Awaited<ReturnType<typeof getSummaryStats>>;
 export default async function page() {
-  await isAuthorized(["ADMIN"]);
-  const serverLogs = await getLogs();
-  const stats = await getSummaryStats();
 
-  return <AdminDashboard serverLogs={serverLogs} />;
+  const serverLogs = await getLogs();
+  const data  = await getAdminStats()
+  return <AdminDashboard serverLogs={serverLogs} statsData={data} />;
 }
