b6e541778fa2bc1c4d78f912aec7ad981712b8ff
EngCamo <learn3038it@gmail.com>
2025-09-08 22:44:55 +0800

    changes

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -4,7 +4,10 @@ import { TierType } from '@/drizzle/schemas'
 import { env } from '@/env/server'
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!,{
+  apiVersion:"2025-02-24.acacia",
+  typescript:true
+})
 export const SubMap:Record<string,TierType>={
   [env.STRIPE_SOLVER_PRODUCT_ID]:"SOLVER",
   [env.STRIPE_SOLVER_PLUS_PRICE_ID]:"SOLVER++",
