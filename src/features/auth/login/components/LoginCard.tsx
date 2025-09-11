cf75a78a5937969e541ac130832528d77cec35f3
EngCamo <learn3038it@gmail.com>
2025-09-12 03:46:30 +0800

    major updates

--- src/features/auth/login/components/LoginCard.tsx
+++ src/features/auth/login/components/LoginCard.tsx
@@ -1,5 +1,12 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import {
   Form,
   FormControl,
@@ -9,19 +16,19 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Loader2 } from "lucide-react";
+import Link from "next/link";
 import { UseFormReturn } from "react-hook-form";
 import { z } from "zod";
-import { loginFormSchema, loginInferedTypes } from "../../server/auth-types";
-import Link from "next/link";
+import SocialButtons from "../../components/socialButtons";
 import FormError from "../../register/components/loginFormError";
 import FormSuccess from "../../register/components/loginFormSuccess";
-import { Loader2 } from "lucide-react";
-import SocialButtons from "../../components/socialButtons";
+import { loginFormSchema, loginInferedTypes } from "../../server/auth-types";
 
 type LoginCardProps = {
   myformController: UseFormReturn<loginInferedTypes>;
   submitHandler: (values: z.infer<typeof loginFormSchema>) => Promise<void>;
-  isPending:boolean
+  isPending: boolean;
   error: string;
   success: string;
 };
@@ -34,68 +41,85 @@ export default function LoginCard({
   success,
 }: LoginCardProps) {
   return (
-    <div className="flex flex-col w-100 pt-2 ">
-      <p className="text-2xl text-foreground font-sans font-semibold py-10">
-        Welcome Back
-      </p>
-      <Card className="flex gap-4 ">
-        <CardContent>
-          <Form {...myformController}>
-            <form
-              onSubmit={myformController.handleSubmit(submitHandler)}
-              className="flex flex-col  gap-4 ">
+    <Card className="flex gap-4 w-full max-w-md">
+      <CardHeader className="space-y-1">
+        <CardTitle className="text-2xl font-bold">Welcome Back</CardTitle>
+        <CardDescription>
+          Enter your credentials to access your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <Form {...myformController}>
+          <form
+            onSubmit={myformController.handleSubmit(submitHandler)}
+            className="flex flex-col  gap-4 ">
+            <FormField
+              control={myformController.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="jonh@gmail.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {
               <FormField
                 control={myformController.control}
-                name="email"
+                name="password"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>Email</FormLabel>
+                    <FormLabel>Password</FormLabel>
                     <FormControl>
-                      <Input placeholder="jonh@gmail.com" {...field} />
+                      <Input type="password" placeholder="******" {...field} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
-              {
-                <FormField
-                  control={myformController.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Password</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="password"
-                          placeholder="******"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              }
-              <FormError message={error} />
-              <FormSuccess message={success} />
-              <Button className="px-28 py-6 mt-4" variant={"default"} disabled={isPending}>
-                {isPending? (<Loader2 className="animate-spin"/>):'Login'}
-              </Button>
-            </form>
-          </Form>
-          <div className="flex gap-4 mb-4 w-full items-center mt-5">
-              <SocialButtons />
-          </div>
-        </CardContent>
-        <CardFooter className="flex place-content-center text-foreground">
-          <p className="text-sm  ">
-            {"Don't have an account? "}
-            <Link className="font-semibold " href={"/register"}>
-              Sign Up Now
-            </Link>
-          </p>
-        </CardFooter>
-      </Card>
+            }
+            <FormError message={error} />
+            <FormSuccess message={success} />
+            <Button
+              className="px-28 py-6 mt-4"
+              variant={"default"}
+              disabled={isPending}>
+              {isPending ? <Loader2 className="animate-spin" /> : "Login"}
+            </Button>
+          </form>
+        </Form>
+
+        <ContinueSeperatorBtn />
+        <div className="flex gap-4 mb-4 w-full items-center mt-5">
+          <SocialButtons />
+        </div>
+      </CardContent>
+      <CardFooter className="flex place-content-center text-foreground">
+        <p className="text-sm  ">
+          {"Don't have an account? "}
+          <Link className="font-semibold " href={"/register"}>
+            Sign Up Now
+          </Link>
+        </p>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export function ContinueSeperatorBtn() {
+  return (
+    <div className="relative">
+      <div className="absolute inset-0 flex items-center">
+        <span className="w-full border-t" />
+      </div>
+      <div className="relative flex justify-center text-xs uppercase">
+        <span className="bg-background px-2 text-muted-foreground">
+          Or continue with
+        </span>
+      </div>
     </div>
   );
 }
