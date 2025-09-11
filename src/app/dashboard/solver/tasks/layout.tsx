7e50c575f79987b2f1144dfbaaba5fa40df325e7
EngCamo <learn3038it@gmail.com>
2025-09-12 03:45:11 +0800

    major updates

--- src/app/dashboard/solver/tasks/layout.tsx
+++ src/app/dashboard/solver/tasks/layout.tsx
@@ -1,24 +1,11 @@
 import {
-  getServerUserSession,
-  isAuthorized,
+  isAuthorized
 } from "@/features/auth/server/actions";
 import { ReactNode } from "react";
-import { getUserById } from "@/features/users/server/actions";
-import { redirect } from "next/navigation";
 
 export default async function Layout({ children }: { children: ReactNode }) {
-  const { user } = await isAuthorized(["SOLVER"]);
+  await isAuthorized(["SOLVER"]);
 
-  if (!user || !user.id) {
-    return redirect("/api/auth/signout?callbackUrl=/login");
-  }
-
-  const userInDb = await getUserById(user.id);
-  if (!userInDb) {
-    return redirect(
-      "/api/auth/signout?callbackUrl=/login?error=account_deleted"
-    );
-  }
 
   return <>{children}</>;
 }
