87a42fd66bf25a171443075dcafdbec0b04f49af
EngCamo <learn3038it@gmail.com>
2025-09-12 03:44:53 +0800

    major updates

--- src/app/(authentication)/login/page.tsx
+++ src/app/(authentication)/login/page.tsx
@@ -1,17 +1,18 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { EmailSignInAction } from "@/features/auth/server/actions";
-import { useState, useTransition } from "react";
+import Logo from "@/components/marketing/logo";
+import FeaturePanelWithAnimation from "@/features/auth/components/feature-panel";
 import LoginCard from "@/features/auth/login/components/LoginCard";
+import { EmailSignInAction } from "@/features/auth/server/actions";
 import {
   loginFormSchema,
   loginInferedTypes,
 } from "@/features/auth/server/auth-types";
-import { useRouter, useSearchParams } from "next/navigation";
-import Logo from "@/components/marketing/logo";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { signIn } from "next-auth/react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
 
 export default function Login() {
   const searchParms = useSearchParams();
@@ -23,7 +24,7 @@ export default function Login() {
   const [error, setError] = useState<string>("");
   const [success, setSuccess] = useState<string>("");
   const [isPending, startTransition] = useTransition();
-  const router = useRouter()
+  const router = useRouter();
   const submitHandler = async (values: loginInferedTypes) => {
     startTransition(async () => {
       setError("");
@@ -34,21 +35,19 @@ export default function Login() {
       const { error, success } = await EmailSignInAction(values);
 
       if (error) return setError(error);
-      if (success && success !== "OK") return setSuccess(success); 
-  
-      
+      if (success && success !== "OK") return setSuccess(success);
+
       const res = await signIn("credentials", {
         email: values.email,
         password: values.password,
         redirect: false,
       });
-  
+
       if (res?.error) {
         setError("Login failed: " + res.error);
       } else {
         router.push("/dashboard");
       }
-    ;
     });
   };
 
@@ -60,24 +59,24 @@ export default function Login() {
     },
   });
   return (
-   
-      <div className="flex w-full h-screen ">
-        <div className="relative bg-sidebar w-full h-screen flex  justify-center items-center">
-          <div className=" absolute top-6 left-6">
-            <Logo />
-          </div>
-
-          <LoginCard
-            myformController={myformController}
-            submitHandler={submitHandler}
-            error={error || oAuthConflictError}
-            success={success}
-            isPending={isPending}
-          />
+    <div className="flex w-full h-screen ">
+      <div className="relative  w-full h-screen flex  justify-center items-center">
+        <div className=" absolute top-6 left-6">
+          <Logo />
         </div>
 
-        <div className=" inset-0 md:flex items-center justify-center text-white  bg-primary font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl w-full hidden"></div>
+        <LoginCard
+          myformController={myformController}
+          submitHandler={submitHandler}
+          error={error || oAuthConflictError}
+          success={success}
+          isPending={isPending}
+        />
       </div>
-   
+
+      <div className="inset-0 md:flex items-center justify-center w-full hidden">
+        <FeaturePanelWithAnimation />
+      </div>
+    </div>
   );
 }
