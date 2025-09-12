c21fbdb55072f0fbc305f546ec775175fabf7f3b
EngCamo <learn3038it@gmail.com>
2025-09-12 10:55:04 +0800

    major updates

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,11 +1,11 @@
-import Hero from "@/components/marketing/hero";
-import Features from "@/components/marketing/features";
 import CTA from "@/components/marketing/cta";
+import DemoSection from "@/components/marketing/DemoSection";
+import Features from "@/components/marketing/features";
 import Footer from "@/components/marketing/footer";
+import Hero from "@/components/marketing/hero";
 import MouseMoveEffect from "@/components/marketing/mouse-move-effect";
 import Navbar from "@/components/marketing/navbar";
 import Pricing from "@/components/marketing/pricing";
-import DemoSection from "@/components/marketing/DemoSection";
 
 
 export default function Home() {
@@ -14,9 +14,9 @@ export default function Home() {
       <MouseMoveEffect />
 
       <div className="pointer-events-none fixed inset-0">
-        <div className="absolute inset-0 bg-gradient-to-b from-background via-background/90 to-background" />
-        <div className="absolute right-0 top-0 h-[500px] w-[500px] bg-blue-500/10 blur-[100px]" />
-        <div className="absolute bottom-0 left-0 h-[500px] w-[500px] bg-purple-500/10 blur-[100px]" />
+        <div className="absolute inset-0 " />
+        <div className="absolute right-0 top-0 h-[500px] w-[500px] blur-[100px]" />
+        <div className="absolute bottom-0 left-0 h-[500px] w-[500px]  blur-[100px]" />
       </div>
 
       <div className="z-10 w-full max-w-7xl px-4 ">
