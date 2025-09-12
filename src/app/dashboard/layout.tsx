2e41781ace4876422bcb170b5982d37aa8bd35bd
EngCamo <learn3038it@gmail.com>
2025-09-12 10:55:10 +0800

    major updates

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,12 @@
 import React, { ReactNode } from "react";
 
 import BridCarmComponent from "@/components/BridCarmComponent";
+import OnboardingForm from "@/components/dashboard/user-onboarding-lazyloaded";
 import WalletDropdownMenu from "@/components/dashboard/WalletDropdownMenu";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { FeatureFlagProvider } from "@/contexts/FeatureFlagContext";
 import ReactQueryProvider from "@/contexts/ReactQueryProvider";
+import { Motion3DBackground } from "@/features/auth/components/feature-panel";
 import {
   getServerSession,
   getServerUserSession,
@@ -53,7 +55,7 @@ export default async function DashboardLayout({
     );
   }
 
-  const subscription = await getServerUserSubscriptionById(id);
+  const subscription = await getServerUserSubscriptionById(userInDb.id);
   let stripeData: StripeSubscriptionContextType = {
     cancelAt: null,
     isCancelScheduled: false,
@@ -89,7 +91,6 @@ export default async function DashboardLayout({
   const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
   const { pending, availabel } = await getWalletInfo(session.user.id);
   const allNotifications = await getAllNotification(userInDb.id!);
-  // logger.info("intial notificaion about to pass",allNotifications)
   return (
     <SessionProvider
       session={session}
@@ -106,34 +107,42 @@ export default async function DashboardLayout({
             } as React.CSSProperties
           }>
           <ReactQueryProvider>
-            <div className="flex h-screen w-full">
-              <DashboardSidebar user={session?.user!} />
-              <div className="flex flex-col flex-1 ">
-                <header className="sticky top-0 z-10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-b">
-                  <div className=" flex h-14 items-center px-4 sm:px-6 justify-between">
-                    <div className="flex items-center">
-                      <SidebarTrigger className="mr-2" />
-                      <BridCarmComponent />
-                    </div>
-
-                    <div className="flex gap-2">
-                      {user.role === "SOLVER" && (
-                        <WalletDropdownMenu
-                          availabel={availabel}
-                          pending={pending}
-                        />
-                      )}
-                      <NotificationDropDown
-                        initailAllNotifications={allNotifications}
-                      />
-                    </div>
-                  </div>
-                </header>
-                <FeatureFlagProvider flags={dbFlags}>
-                  {children}
-                </FeatureFlagProvider>
+            {(userInDb.role === "SOLVER" || userInDb.role === "POSTER") &&
+            !userInDb?.userDetails.onboardingCompleted ? (
+              <div className="relative flex flex-col justify-center items-center  h-screen w-full bg-gradient-to-br from-primary/5 via-background to-accent/10 overflow-hidden">
+                <Motion3DBackground />
+                <OnboardingForm />
               </div>
-            </div>
+            ) : (
+              <div className="flex h-screen w-full">
+                <DashboardSidebar user={session?.user!} />
+                <div className="flex flex-col flex-1 ">
+                  <header className="sticky top-0 z-10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-b">
+                    <div className=" flex h-14 items-center px-4 sm:px-6 justify-between">
+                      <div className="flex items-center">
+                        <SidebarTrigger className="mr-2" />
+                        <BridCarmComponent />
+                      </div>
+
+                      <div className="flex gap-2">
+                        {user.role === "SOLVER" && (
+                          <WalletDropdownMenu
+                            availabel={availabel}
+                            pending={pending}
+                          />
+                        )}
+                        <NotificationDropDown
+                          initailAllNotifications={allNotifications}
+                        />
+                      </div>
+                    </div>
+                  </header>
+                  <FeatureFlagProvider flags={dbFlags}>
+                    {children}
+                  </FeatureFlagProvider>
+                </div>
+              </div>
+            )}
           </ReactQueryProvider>
         </SidebarProvider>
       </StripeSubscriptionProvider>
