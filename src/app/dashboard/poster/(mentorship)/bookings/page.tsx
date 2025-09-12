9a004547fa64aace8b62046cb7a1f3367abac479
EngCamo <learn3038it@gmail.com>
2025-09-12 10:55:14 +0800

    major updates

--- src/app/dashboard/poster/(mentorship)/bookings/page.tsx
+++ src/app/dashboard/poster/(mentorship)/bookings/page.tsx
@@ -1,12 +1,24 @@
-import { MentorsBrowser } from "@/features/mentore/components/BrowseMentor"
-import {  getMentorListigWithAvailbelDates} from "@/features/mentore/server/action"
+import { MentorsBrowser } from "@/features/mentore/components/BrowseMentor";
+import {
+  cleanPendingBookign,
+  getMentorListigWithAvailbelDates,
+} from "@/features/mentore/server/action";
+import { redirect } from "next/navigation";
 
-export  default async function MentorsPage() {
-  const mentorWithj = await getMentorListigWithAvailbelDates()
-
+export default async function MentorsPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ booking_id: string }>;
+}) {
+  const { booking_id } = await searchParams;
+  const mentorWithj = await getMentorListigWithAvailbelDates();
+  if (booking_id) {
+    await cleanPendingBookign(booking_id);
+    redirect("/dashboard/poster/bookings");
+  }
   return (
     <div className="container mx-auto px-4 py-8">
-      <MentorsBrowser mentors={mentorWithj}  />
+      <MentorsBrowser mentors={mentorWithj} />
     </div>
-  )
+  );
 }
