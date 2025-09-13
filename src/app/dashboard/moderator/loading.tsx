aac7efbafc2b5867522cb229ef675eced295fb09
EngCamo <learn3038it@gmail.com>
2025-09-14 04:44:30 +0800

    Major updates and refactoring

--- src/app/dashboard/moderator/loading.tsx
+++ src/app/dashboard/moderator/loading.tsx
@@ -1,5 +1,9 @@
-import DashboardSkeleton from "../loading";
+import DashboardLoading from "@/components/dashboard/dashboard-loading";
 
-export default function Loading() {
-  return <DashboardSkeleton />;
+export default function oading() {
+  return (
+    <main className="flex flex-col flex-1 ">
+      <DashboardLoading />
+    </main>
+  );
 }
