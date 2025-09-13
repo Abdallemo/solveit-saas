f4c89f2551a4984f71ed19051e1c9ffa217c0154
EngCamo <learn3038it@gmail.com>
2025-09-14 07:14:42 +0800

    deadline traced added

--- src/app/dashboard/solver/page.tsx
+++ src/app/dashboard/solver/page.tsx
@@ -1,13 +1,17 @@
 import { isAuthorized } from "@/features/auth/server/actions";
-import { getSolverStats } from "@/features/tasks/server/data";
+import {
+  getSolverStats,
+  getSolverUpcomminDeadlines,
+} from "@/features/tasks/server/data";
 import SolverDashboard from "@/features/users/components/solver/SolverDashboard";
 
 export default async function page() {
   await isAuthorized(["SOLVER"]);
   const data = await getSolverStats();
+  const deadlines = await getSolverUpcomminDeadlines();
   return (
     <div className="w-full h-full">
-      <SolverDashboard stats={data} />
+      <SolverDashboard stats={data} deadlines={deadlines} />
     </div>
   );
 }
