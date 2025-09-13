78277ad4cb48ddf3d7a42d4daa0c04c6e685b710
EngCamo <learn3038it@gmail.com>
2025-09-13 23:04:46 +0800

    Major updates and refactoring

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,4 @@
 import type { NextConfig } from "next";
-import withBundleAnalyzer from "@next/bundle-analyzer";
-
-const withAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-});
 
 const nextConfig: NextConfig = {
   images: {
@@ -27,4 +22,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withAnalyzer(nextConfig);
+export default nextConfig;
