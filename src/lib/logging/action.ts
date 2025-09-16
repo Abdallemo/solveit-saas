639af3abfdeb3c1b526215a3c31b4072abc46109
EngCamo <learn3038it@gmail.com>
2025-09-16 23:38:47 +0800

    changes

--- src/lib/logging/action.ts
+++ src/lib/logging/action.ts
@@ -4,8 +4,8 @@ import db from "@/drizzle/db";
 export type LogsTyep = Awaited<ReturnType<typeof getLogs>>
 export async function getLogs() {
   const logs =await db.query.logTable.findMany({
-    orderBy: (l, { desc }) => desc(l.timestamp),
-    columns: { id: true, level: true, message: true ,timestamp:true},
+    orderBy: (l, { desc }) => desc(l.createdAt),
+    columns: { id: true, level: true, message: true ,createdAt:true},
   });
   return logs
 }
