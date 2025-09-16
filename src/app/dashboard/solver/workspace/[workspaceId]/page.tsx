5c80a4a585a7baefd0f37cfc4d1056a45a6f54fc
EngCamo <learn3038it@gmail.com>
2025-09-16 23:38:32 +0800

    changes

--- src/app/dashboard/solver/workspace/[workspaceId]/page.tsx
+++ src/app/dashboard/solver/workspace/[workspaceId]/page.tsx
@@ -1,7 +1,12 @@
-import WorkspacePageComp from "@/features/tasks/components/WorkspacePageComp";
 import { isAuthorized } from "@/features/auth/server/actions";
+import WorkspacePageComp from "@/features/tasks/components/WorkspacePageComp";
+import { Suspense } from "react";
+import WorksapceSkeleton from "./loading";
 export default async function Page() {
   await isAuthorized(["SOLVER"]);
-
-  return <WorkspacePageComp />;
+  return (
+    <Suspense fallback={<WorksapceSkeleton />}>
+      <WorkspacePageComp />
+    </Suspense>
+  );
 }
