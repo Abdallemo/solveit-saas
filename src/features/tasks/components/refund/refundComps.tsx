be517dcee2c55c2a6fdc6e6c05b054da01072a2b
EngCamo <learn3038it@gmail.com>
2025-09-18 09:06:03 +0800

    changes

--- src/features/tasks/components/refund/refundComps.tsx
+++ src/features/tasks/components/refund/refundComps.tsx
@@ -1,29 +1,17 @@
 "use client";
 
 import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-  getPaginationRowModel,
-  getSortedRowModel,
-  SortingState,
-  ColumnFiltersState,
-  getFilteredRowModel,
-  VisibilityState,
-} from "@tanstack/react-table";
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { allDisputesType } from "../../server/task-types";
-import { GetRefundStatusBadge } from "../taskStatusBadge";
-import { RefundStatusEnumType } from "@/drizzle/schemas";
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
@@ -34,131 +22,193 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
-import { MoreHorizontal, ArrowUpDown, ChevronDown } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { RefundStatusEnumType } from "@/drizzle/schemas";
+import { refundPoster } from "@/features/payments/server/action";
+import { useMutation } from "@tanstack/react-query";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+  VisibilityState,
+} from "@tanstack/react-table";
+import {
+  ArrowUpDown,
+  ChevronDown,
+  Loader2,
+  MoreHorizontal,
+} from "lucide-react";
+import { useMemo, useState } from "react";
 import { toast } from "sonner";
-import { useState } from "react";
-export const Columns: ColumnDef<allDisputesType>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
-    accessorKey: "posterName",
-    header: "Poster",
-  },
-  {
-    accessorKey: "posterEmail",
-    header: "Poster Email",
-  },
-  {
-    accessorKey: "refundReason",
-    header: "Refund Reason",
-  },
-  {
-    accessorKey: "refundStatus",
-    header: "Refund Status",
-    cell: ({ row }) => {
-      const status = row.getValue("refundStatus");
-      return GetRefundStatusBadge(status as RefundStatusEnumType);
+import { allDisputesType } from "../../server/task-types";
+import { GetRefundStatusBadge } from "../taskStatusBadge";
+function NewRefundColumns({
+  handleOpenDialog,
+}: {
+  handleOpenDialog: (dispute: allDisputesType) => void;
+}): ColumnDef<allDisputesType>[] {
+  return [
+    {
+      id: "select",
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
     },
-  },
-  {
-    accessorKey: "solverName",
-    header: "Solver",
-  },
-  {
-    accessorKey: "solverEmail",
-    header: "Solver Email",
-  },
-  {
-    accessorKey: "refundedAt",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant={"ghost"}
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-          Refunded At <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      );
+    {
+      accessorKey: "posterName",
+      header: "Poster",
     },
-    cell: ({ row }) => {
-      return new Intl.DateTimeFormat("en-US", {
-        year: "numeric",
-        month: "numeric",
-        day: "numeric",
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: true,
-      }).format(row.getValue("refundedAt"));
+    {
+      accessorKey: "posterEmail",
+      header: "Poster Email",
     },
-  },
-  {
-    accessorKey: "taskPrice",
-    header: "Task Price",
-  },
-  {
-    id: "Actions",
-    cell: ({ row }) => {
-      const dispute = row.original;
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => {
-                navigator.clipboard.writeText(dispute.id);
-                toast.info("copie to clipboard");
-              }}>
-              Copy Dispute ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View Solver</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
+    {
+      accessorKey: "refundReason",
+      header: "Refund Reason",
     },
-  },
-];
+    {
+      accessorKey: "refundStatus",
+      header: "Refund Status",
+      cell: ({ row }) => {
+        const status = row.getValue("refundStatus");
+        return GetRefundStatusBadge(status as RefundStatusEnumType);
+      },
+    },
+    {
+      accessorKey: "solverName",
+      header: "Solver",
+    },
+    {
+      accessorKey: "solverEmail",
+      header: "Solver Email",
+    },
+    {
+      accessorKey: "refundedAt",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant={"ghost"}
+            onClick={() =>
+              column.toggleSorting(column.getIsSorted() === "asc")
+            }>
+            Refunded At <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        );
+      },
+      cell: ({ row }) => {
+        return new Date(row.getValue("refundedAt")).toLocaleTimeString(
+          undefined,
+          {
+            day: "2-digit",
+            hour: "2-digit",
+            month: "2-digit",
+            minute: "2-digit",
+            year: "numeric",
+          }
+        );
+      },
+    },
+    {
+      accessorKey: "taskPrice",
+      header: "Task Price",
+    },
+    {
+      id: "Actions",
+      cell: ({ row }) => {
+        const dispute = row.original;
 
-interface RefundTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[];
-  data: TData[];
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() => {
+                  navigator.clipboard.writeText(dispute.id);
+                  toast.info("copie to clipboard");
+                }}>
+                Copy Dispute ID
+              </DropdownMenuItem>
+              {dispute.refundStatus === "PENDING" && (
+                <DropdownMenuItem
+                  onSelect={(e) => {
+                    e.preventDefault();
+                  }}
+                  onClick={() => handleOpenDialog(dispute)}>
+                  Accept Refund
+                </DropdownMenuItem>
+              )}
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>View Solver</DropdownMenuItem>
+              <DropdownMenuItem>View payment details</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      },
+    },
+  ];
 }
-export function RefundTable<TData, TValue>({
-  columns,
-  data,
-}: RefundTableProps<TData, TValue>) {
+
+interface RefundTableProps {
+  data: allDisputesType[];
+}
+export function RefundTable({ data }: RefundTableProps) {
   const [sorting, setSorting] = useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
   const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
   const [rowSelection, setRowSelection] = useState({});
-
+  const [open, setOpen] = useState(false);
+  const [selectedRefund, setSelectedRefund] = useState<allDisputesType | null>(
+    null
+  );
+  const [confirmInput, setConfirmInput] = useState("");
+  const { mutateAsync: issueRefund, isPending: isRefunding } = useMutation({
+    mutationFn: refundPoster,
+    onError: (e) => toast.error(e.message),
+    onSuccess:()=>toast.success("Successfully refuned")
+  });
+  const columns = useMemo(() => {
+    const handleOpenDialog = (dispute: allDisputesType) => {
+      setSelectedRefund(dispute);
+      setOpen(true);
+      setConfirmInput("");
+    };
+    return NewRefundColumns({ handleOpenDialog });
+  }, []);
   const table = useReactTable({
     data,
     columns,
@@ -280,6 +330,34 @@ export function RefundTable<TData, TValue>({
           Next
         </Button>
       </div>
+      {selectedRefund && (
+        <AlertDialog open={open} onOpenChange={setOpen}>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Accept This Refund?</AlertDialogTitle>
+              <AlertDialogDescription className="text-foreground">
+                By accepting this refund, you confirm that you are in favor of
+                the poster. This action will mark the task as refunded and issue
+                a refund or task re-open to the poster.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel disabled={isRefunding} className={``}>
+                Cancel
+              </AlertDialogCancel>
+              <AlertDialogAction
+                disabled={isRefunding}
+                onClick={async () =>
+                  await issueRefund(selectedRefund.taskPaymentId!)
+                }
+                className={`${"cursor-not-allowed"} bg-emerald-600 text-white hover:bg-emerald-700 focus:ring-emerald-500 cursor-pointer`}>
+                {isRefunding && <Loader2 className="animate-spin" />}
+                Yes, Accept Solution
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      )}
     </div>
   );
 }
