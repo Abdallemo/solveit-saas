002b0b4c04c24526396fe284ae917801a00f3121
EngCamo <learn3038it@gmail.com>
2025-09-20 00:46:48 +0800

    changes

--- src/routes.ts
+++ src/routes.ts
@@ -1,30 +1,30 @@
 /**
  * an array of routes that is accessible to the public
  * these dont require authenitcaton nor authorization
- * @type {string[]}
  */
-export const publicRoutes =[
-    '/',
-    
-]
+export const publicRoutes = [
+  "/",
+  "/about",
+  "/contact",
+  "/blog",
+  "/resources",
+  "/faq",
+];
 /**
  * an array of routes that used for authentication
  * these will redirect loggin users to their dashboard
- * @type {string[]}
  */
-export const authRoutes =[
-    '/login',
-    '/register',
-    '/login/error',
-    '/login/verify'
+export const authRoutes = [
+  "/login",
+  "/register",
+  "/login/error",
+  "/login/verify",
+];
 
+export const apiAuthPrefix = "/api/auth";
+export const apiStripePrefix = "/api/webhooks/stripe";
+export const apiStripePrefixPayment = "/api/webhooks/stripe/taskPayment";
+export const apiMediaPrefix = "/api/media";
+export const apiLogsPrefix = "/api/logs";
 
-]
-
-export const apiAuthPrefix = '/api/auth'
-export const apiStripePrefix = '/api/webhooks/stripe'
-export const apiStripePrefixPayment = '/api/webhooks/stripe/taskPayment'
-export const apiMediaPrefix = '/api/media'
-export const apiLogsPrefix = '/api/logs'
-
-export const DEFAULT_LOGIN_REDIRECT = '/dashboard'
+export const DEFAULT_LOGIN_REDIRECT = "/dashboard";
