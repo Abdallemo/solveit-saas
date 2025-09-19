12c01b3e975b56281f58b2b821847aced1a579ba
EngCamo <learn3038it@gmail.com>
2025-09-19 10:41:36 +0800

    changes

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import OnboardingForm from "@/components/dashboard/user-onboarding-lazyloaded";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-background flex items-center justify-center p-4">
-      <OnboardingForm />
-    </main>
-  );
-}
