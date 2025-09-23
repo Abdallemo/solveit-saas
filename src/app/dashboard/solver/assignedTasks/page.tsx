adc1e4ea81869c853f18b085b623ef3024698c23
EngCamo <learn3038it@gmail.com>
2025-09-24 07:33:00 +0800

    Changes

--- src/app/dashboard/solver/assignedTasks/page.tsx
+++ src/app/dashboard/solver/assignedTasks/page.tsx
@@ -1,39 +1,43 @@
+import { TaskStatusType } from "@/drizzle/schemas";
 import { getServerUserSession } from "@/features/auth/server/actions";
 import DisplayListComponent from "@/features/tasks/components/DisplayComponent";
 import { handleTaskDeadline } from "@/features/tasks/server/action";
-import { getAllCategoryMap, getAssignedTasksbyIdPaginated } from "@/features/tasks/server/data";
-
+import {
+  getAllCategoryMap,
+  getAssignedTasksbyIdPaginated,
+} from "@/features/tasks/server/data";
 
 export default async function ServerWrapper({
   searchParams,
 }: {
-  searchParams: Promise<{ q: string; page: string }>
+  searchParams: Promise<{ search: string; page: string; status: string }>;
 }) {
-  const currentUser = await getServerUserSession()
-  if (!currentUser || !currentUser.role || !currentUser.id) return
+  const currentUser = await getServerUserSession();
+  if (!currentUser || !currentUser.role || !currentUser.id) return;
 
-  const categoryMap = await getAllCategoryMap()
-  const { q, page } = await searchParams
-  const search = q ?? ""
-  const pages = Number.parseInt(page ?? "1")
-  const limit = 8
-  const offset = (pages - 1) * limit
+  const categoryMap = await getAllCategoryMap();
+  const { search, page,status } = await searchParams;
 
-   const { tasks, totalCount } = await getAssignedTasksbyIdPaginated(
+  const pages = Number.parseInt(page ?? "1");
+  const limit = 8;
+  const offset = (pages - 1) * limit;
+
+  const { tasks, totalCount } = await getAssignedTasksbyIdPaginated(
     currentUser.id,
     {
-      search,
+      search:search ?? "",
       limit,
       offset,
+      status: status as TaskStatusType ?? "",
     },
     true
   );
   for (const task of tasks) {
-    await handleTaskDeadline(task)
+    await handleTaskDeadline(task);
   }
-  const totalPages = Math.ceil(totalCount / limit)
-  const hasPrevious = pages > 1
-  const hasNext = pages < totalPages
+  const totalPages = Math.ceil(totalCount / limit);
+  const hasPrevious = pages > 1;
+  const hasNext = pages < totalPages;
   return (
     <DisplayListComponent
       title="Assigned Tasks"
@@ -45,7 +49,6 @@ export default async function ServerWrapper({
       hasPrevious={hasPrevious}
       hasNext={hasNext}
       filterType="status"
-
     />
-  )
+  );
 }
