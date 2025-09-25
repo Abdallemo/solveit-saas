03254aca6621f4f8072cf5006c64744306dff70a
EngCamo <learn3038it@gmail.com>
2025-09-26 05:40:16 +0800

    added video call feature

--- src/app/dashboard/solver/(mentorship)/mentor/[sessionId]/@modal/(.)video-call/page.tsx
+++ src/app/dashboard/solver/(mentorship)/mentor/[sessionId]/@modal/(.)video-call/page.tsx
@@ -1,10 +1,17 @@
+import { isAuthorized } from "@/features/auth/server/actions";
 import { VideoCallModal } from "@/features/mentore/components/workspace/VideoCallModal";
 import { VideoCallPageComps } from "@/features/mentore/components/workspace/VideoCallpageComps";
 
-export default function page() {
+export default async function page({
+  params,
+}: {
+  params: Promise<{ sessionId: string }>;
+}) {
+  const { user } = await isAuthorized(["SOLVER"]);
+  const { sessionId } = await params;
   return (
     <VideoCallModal>
-      <VideoCallPageComps />
+      <VideoCallPageComps userId={user.id} sessionId={sessionId} />
     </VideoCallModal>
   );
 }
