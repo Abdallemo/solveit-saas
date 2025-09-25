c45f014d48bdd9a4790a71a96e397d46e9a0c738
EngCamo <learn3038it@gmail.com>
2025-09-26 05:40:17 +0800

    added video call feature

--- src/features/mentore/components/workspace/VideoCallpageComps.tsx
+++ src/features/mentore/components/workspace/VideoCallpageComps.tsx
@@ -1,11 +1,91 @@
 "use client";
 
-import { Video } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useMentorshipCall } from "@/hooks/use-video-call";
+import { Mic, MicOff, PhoneOff, User, Video, VideoOff } from "lucide-react";
+import { useRouter } from "next/navigation";
 
-export function VideoCallPageComps() {
+export function VideoCallPageComps({
+  userId,
+  sessionId,
+}: {
+  userId: string;
+  sessionId: string;
+}) {
+  const {
+    localVideo,
+    remoteVideo,
+    cameraOn,
+    micOn,
+    setCameraOn,
+    setMicOn,
+    endCall,
+  } = useMentorshipCall(userId, sessionId as string);
+  const router = useRouter();
   return (
-    <div className="flex flex-col w-full h-full justify-center items-center overflow-hidden rounded-lg border border-white/5">
-      <Video />
+    <div className="flex flex-col w-full h-full justify-between items-center rounded-lg border border-white/5 ">
+      <div className="flex flex-col md:flex-row gap-2 w-full h-full p-2">
+        <div className="flex-1 relative rounded-lg overflow-hidden ">
+          {localVideo ? (
+            <video
+              ref={localVideo}
+              autoPlay
+              playsInline
+              muted
+              className="w-full h-full object-cover"
+            />
+          ) : (
+            <div className="flex flex-col h-full w-full items-center justify-center text-gray-400">
+              <VideoOff size={48} />
+              <p>No camera</p>
+            </div>
+          )}
+          <div className="absolute bottom-2 left-2 text-xs bg-black/50 px-2 py-1 rounded">
+            You
+          </div>
+        </div>
+
+        <div className="flex-1 relative rounded-lg overflow-hidden bg-foreground/50">
+          {remoteVideo ? (
+            <video
+              ref={remoteVideo}
+              autoPlay
+              playsInline
+              className="w-full h-full object-cover"
+            />
+          ) : (
+            <div className="flex flex-col h-full w-full items-center justify-center ">
+              <User size={48} />
+              <p>Waiting for peer to join...</p>
+            </div>
+          )}
+          <div className="absolute bottom-2 left-2 text-xs bg-black/50 px-2 py-1 rounded">
+            Peer
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center gap-4 py-4  w-full">
+        <Button variant="ghost" size="icon" onClick={() => setMicOn(!micOn)}>
+          {micOn ? <Mic /> : <MicOff className="text-red-500" />}
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => setCameraOn(!cameraOn)}>
+          {cameraOn ? <Video /> : <VideoOff className="text-red-500" />}
+        </Button>
+        <Button
+          variant="destructive"
+          size="icon"
+          className="rounded-full"
+          onClick={() => {
+            endCall();
+            router.back();
+          }}>
+          <PhoneOff />
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
