5426fe8858f2b6c106e4a0e08a289b04929484a7
EngCamo <learn3038it@gmail.com>
2025-09-26 12:30:12 +0800

    added video call feature

--- src/app/dashboard/solver/(mentorship)/mentor/page.tsx
+++ src/app/dashboard/solver/(mentorship)/mentor/page.tsx
@@ -1,12 +1,9 @@
-import { isAuthorized } from "@/features/auth/server/actions";
 import { SessionsList } from "@/features/mentore/components/MentroSessionPageCompx";
 import {
   getMentorBookingSessions
 } from "@/features/mentore/server/action";
 
 export default async function Page() {
-  const { user } = await isAuthorized(["SOLVER"]);
-
   const bookings = await getMentorBookingSessions();
   return <SessionsList booking={bookings} />;
 }
