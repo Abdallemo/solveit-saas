571eb17370052f6efa637af361d59483e12cb551
EngCamo <learn3038it@gmail.com>
2025-09-26 09:35:44 +0800

    done

--- src/app/dashboard/solver/(mentorship)/mentor/page.tsx
+++ src/app/dashboard/solver/(mentorship)/mentor/page.tsx
@@ -1,19 +1,12 @@
 import { isAuthorized } from "@/features/auth/server/actions";
 import { SessionsList } from "@/features/mentore/components/MentroSessionPageCompx";
 import {
-  getMentorAllSessionCount,
-  getMentorBookingSessions,
+  getMentorBookingSessions
 } from "@/features/mentore/server/action";
 
 export default async function Page() {
   const { user } = await isAuthorized(["SOLVER"]);
-  const data = await getMentorAllSessionCount(user.id);
 
   const bookings = await getMentorBookingSessions();
-  return (
-    <div className="w-full h-full mx-auto p-10   ">
-      <h1 className="text-2xl font-semibold mb-6">Your Sessions</h1>
-      <SessionsList booking={bookings} />
-    </div>
-  );
+  return <SessionsList booking={bookings} />;
 }
