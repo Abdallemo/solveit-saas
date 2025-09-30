2d668ebc03a41c4a5c06a3618ff23118fbaf44fd
EngCamo <learn3038it@gmail.com>
2025-09-30 12:30:41 +0800

    enhaced mentor session with timezone awareness

--- src/app/dashboard/poster/(mentorship)/bookings/page.tsx
+++ src/app/dashboard/poster/(mentorship)/bookings/page.tsx
@@ -2,6 +2,7 @@ import { MentorsBrowser } from "@/features/mentore/components/BrowseMentor";
 import {
   cleanPendingBookign,
   getMentorListigWithAvailbelDates,
+  getMentorListigWithAvailbelDatesV2,
 } from "@/features/mentore/server/action";
 import { redirect } from "next/navigation";
 
@@ -12,13 +13,15 @@ export default async function MentorsPage({
 }) {
   const { booking_id } = await searchParams;
   const mentorWithj = await getMentorListigWithAvailbelDates();
+  const mentorWithjV2 = await getMentorListigWithAvailbelDatesV2();
+  // console.log(mentorWithjV2[0].availableDates)
   if (booking_id) {
     await cleanPendingBookign(booking_id);
     redirect("/dashboard/poster/bookings");
   }
   return (
     <div className="container mx-auto px-4 py-8">
-      <MentorsBrowser mentors={mentorWithj} />
+      <MentorsBrowser mentors={mentorWithjV2} />
     </div>
   );
 }
