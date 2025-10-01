8239b7b740459acb8fe49bb9b161c8d7c15176b6
EngCamo <learn3038it@gmail.com>
2025-10-01 10:44:42 +0800

    enhaced mentor session

--- src/app/dashboard/poster/(mentorship)/sessions/[sessionId]/page.tsx
+++ src/app/dashboard/poster/(mentorship)/sessions/[sessionId]/page.tsx
@@ -1,14 +1,5 @@
 import MentorshipWorkspace from "@/features/mentore/components/workspace/MentorshipWorkspace";
-import { getMentorSession } from "@/features/mentore/server/action";
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ sessionId: string }>;
-}) {
-  const { sessionId } = await params;
-  const session = await getMentorSession(sessionId)
-  // if (!session)return
-  
-  return <MentorshipWorkspace mentorWorkspace={session}/>;
+export default async function Page() {
+  return <MentorshipWorkspace />;
 }
