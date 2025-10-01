b6ff08aa946fb66768db96a40d2f87b1b4d6c39c
EngCamo <learn3038it@gmail.com>
2025-10-01 17:24:06 +0800

    Overal admin side changes

--- src/features/subscriptions/server/db.ts
+++ src/features/subscriptions/server/db.ts
@@ -1,27 +1,29 @@
 import db from "@/drizzle/db";
-import { eq, SQL } from "drizzle-orm";
-import { UserRole } from "../../../../types/next-auth";
-import { UserTable, UserSubscriptionTable } from "@/drizzle/schemas";
+import { UserSubscriptionTable, UserTable } from "@/drizzle/schemas";
 import { logger } from "@/lib/logging/winston";
+import { eq, isNotNull, SQL } from "drizzle-orm";
+import { UserRole } from "../../../../types/next-auth";
 
 export async function updateUserSubscription(
   values: typeof UserSubscriptionTable.$inferInsert,
   role: UserRole,
   id: string,
-  stripeCustomerId:string
+  stripeCustomerId: string
 ) {
   try {
     await db.transaction(async (dx) => {
       await dx
         .update(UserSubscriptionTable)
         .set(values)
-        .where(eq(UserSubscriptionTable.userId, id));
+        .where(eq(UserSubscriptionTable.userId, id))
+        .returning();
       await dx
         .update(UserTable)
-        .set({ role: role,stripeCustomerId: stripeCustomerId })
+        .set({ role: role, stripeCustomerId: stripeCustomerId })
         .where(eq(UserTable.id, id));
     });
-    logger.info(`Sucessfully Updated User ${id}'s Subscription`)
+
+    logger.info(`Sucessfully Updated User ${id}'s Subscription`);
   } catch (error) {
     logger.error(
       "unable to update user subscription cause: ",
@@ -47,7 +49,7 @@ export async function CancelUserSubscription(
 
     await dx
       .update(UserTable)
-      .set({ role: "POSTER",stripeCustomerId:null })
+      .set({ role: "POSTER", stripeCustomerId: null })
       .where(eq(UserTable.id, id!));
   });
 }
@@ -56,4 +58,24 @@ export async function CreateUserSubsciption(
   values: typeof UserSubscriptionTable.$inferInsert
 ) {
   await db.insert(UserSubscriptionTable).values(values);
+}
+export type Subscription = Awaited<
+  ReturnType<typeof getAllSubscriptions>
+>[number];
+export async function getAllSubscriptions() {
+  return await db
+    .select({
+      id: UserSubscriptionTable.id,
+      customerId: UserTable.stripeCustomerId,
+      customerName: UserTable.name,
+      customerEmail: UserTable.email,
+      planName: UserSubscriptionTable.tier,
+      status: UserSubscriptionTable.status,
+      amount: UserSubscriptionTable.price,
+      interval: UserSubscriptionTable.interval,
+      nextBilling: UserSubscriptionTable.nextBilling,
+    })
+    .from(UserSubscriptionTable)
+    .leftJoin(UserTable, eq(UserSubscriptionTable.userId, UserTable.id))
+    .where(isNotNull(UserSubscriptionTable.stripeSubscriptionId));
 }
