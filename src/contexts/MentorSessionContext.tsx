41c1b7220b5e25d68cfead36038dc99c6655a916
EngCamo <learn3038it@gmail.com>
2025-10-02 15:15:46 +0800

    Overal admin side changes

--- src/contexts/MentorSessionContext.tsx
+++ src/contexts/MentorSessionContext.tsx
@@ -1,10 +1,16 @@
 "use client";
 import { MentorSession } from "@/features/mentore/server/types";
 import { SessionNotFoundError } from "@/lib/Errors";
-import { createContext, ReactNode, useContext, useState } from "react";
-
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useState
+} from "react";
+type PartialMentorSession = Partial<MentorSession>;
 type MentorshipSessionContextType = {
   mentorshipSession: MentorSession;
+  updateSession: (updates: PartialMentorSession) => void;
 };
 
 const MentorshipSessionContext = createContext<
@@ -18,12 +24,16 @@ export const MentorshipSessionProvider = ({
   children,
   mentorshipSession,
 }: MentorshipSessionPorviderProps) => {
-  const [session] = useState(mentorshipSession);
-   if (!session) throw new SessionNotFoundError();
+  const [session, setSession] = useState(mentorshipSession);
+  if (!session) throw new SessionNotFoundError();
+  const updateSession = (updates: PartialMentorSession) => {
+    setSession((prev) => ({ ...prev!, ...updates }));
+  };
   return (
     <MentorshipSessionContext.Provider
       value={{
         mentorshipSession: session,
+        updateSession,
       }}>
       {children}
     </MentorshipSessionContext.Provider>
