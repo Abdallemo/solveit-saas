14108a62ebe9fb97f56f3b06dbeec05f0b3d67f8
EngCamo <learn3038it@gmail.com>
2025-10-08 15:52:39 +0800

    migration changes

--- src/features/mentore/components/workspace/VideoCallClient.tsx
+++ src/features/mentore/components/workspace/VideoCallClient.tsx
@@ -1,9 +1,22 @@
 "use client";
 
+import Gates from "@/components/GateComponents";
+import { useMentorshipSession } from "@/contexts/MentorSessionContext";
+import { sessionUtilsV2 } from "@/lib/utils";
+
 export function VideoCallClientWraper({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="">{children}</div>;
+  const { mentorshipSession } = useMentorshipSession();
+  const isPostSession = sessionUtilsV2.isAfterSession(
+    { sessionEnd: mentorshipSession?.sessionEnd! },
+    new Date()
+  );
+  
+  if (isPostSession) {
+    return <Gates.PostSession sessionId={mentorshipSession?.id!} />;
+  }
+  return <div className="w-full h-full">{children}</div>;
 }
