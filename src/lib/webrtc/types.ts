ef711b7fae389e6a0db2ba71a5a0cfabc89e726a
EngCamo <learn3038it@gmail.com>
2025-10-10 04:03:45 +0800

    webrtc changes

--- src/lib/webrtc/types.ts
+++ src/lib/webrtc/types.ts
@@ -2,7 +2,14 @@ export type connType = "camera" | "screen";
 export type SignalMessage = {
   from: string;
   to: string;
-  type: "offer" | "answer" | "candidate" | "leave" | "stopScreen";
+  type:
+    | "offer"
+    | "answer"
+    | "candidate"
+    | "leave"
+    | "stopScreen"
+    | "startScreen"
+    | "cancelScreen";
   payload: any;
   sessionId: string;
   connectionType?: connType;
