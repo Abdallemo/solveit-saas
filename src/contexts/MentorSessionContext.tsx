76d205e73867d77ced666587e5becb279811d239
EngCamo <learn3038it@gmail.com>
2025-10-11 02:35:39 +0800

    Add persistent floating video UI via Zustand global state Implements a global Zustand store to cache live WebRTC stream data, decoupling it from unmounting route components. This fixes state loss on navigation and enables the display of a persistent, minimized video call interface in the main chat view.

--- src/contexts/MentorSessionContext.tsx
+++ src/contexts/MentorSessionContext.tsx
@@ -1,16 +1,19 @@
 "use client";
-import { MentorSession } from "@/features/mentore/server/types";
-import { SessionNotFoundError } from "@/lib/Errors";
+import { env } from "@/env/client";
 import {
-  createContext,
-  ReactNode,
-  useContext,
-  useState
-} from "react";
+  MentorChatSession,
+  MentorSession,
+} from "@/features/mentore/server/types";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useWebSocket from "@/hooks/useWebSocket";
+import { SessionNotFoundError } from "@/lib/Errors";
+import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useState } from "react";
 type PartialMentorSession = Partial<MentorSession>;
 type MentorshipSessionContextType = {
   mentorshipSession: MentorSession;
   updateSession: (updates: PartialMentorSession) => void;
+  uploadingFiles: File[];
+  setUploadingFiles: Dispatch<SetStateAction<File[]>>
 };
 
 const MentorshipSessionContext = createContext<
@@ -25,15 +28,38 @@ export const MentorshipSessionProvider = ({
   mentorshipSession,
 }: MentorshipSessionPorviderProps) => {
   const [session, setSession] = useState(mentorshipSession);
+  const [uploadingFiles, setUploadingFiles] = useState<File[]>([]);
+  const { user } = useCurrentUser();
+
   if (!session) throw new SessionNotFoundError();
   const updateSession = (updates: PartialMentorSession) => {
     setSession((prev) => ({ ...prev!, ...updates }));
   };
+
+  useWebSocket<MentorChatSession>(
+    `${env.NEXT_PUBLIC_GO_API_WS_URL}/mentorship?session_id=${session?.id}`,
+    {
+      onMessage: (msg) => {
+        updateSession({
+          chats: [
+            ...(session?.chats || []),
+            { ...msg, createdAt: new Date(msg.createdAt!), readAt: null },
+          ],
+        });
+        if (msg.chatFiles?.length > 0 && msg.sentBy === user?.id) {
+          setUploadingFiles([]);
+        }
+      },
+    }
+  );
+
   return (
     <MentorshipSessionContext.Provider
       value={{
         mentorshipSession: session,
         updateSession,
+        uploadingFiles,
+        setUploadingFiles
       }}>
       {children}
     </MentorshipSessionContext.Provider>
