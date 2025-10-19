3011c105253579791fe004eb6880a5c6524509f5
EngCamo <learn3038it@gmail.com>
2025-10-20 04:32:50 +0800

    added file preview features and somee tweaks

--- src/features/media/server/action.ts
+++ src/features/media/server/action.ts
@@ -4,20 +4,41 @@ import { env } from "@/env/server";
 import { GoHeaders } from "@/lib/go-config";
 import { logger } from "@/lib/logging/winston";
 
-
 export async function deleteFileFromR2(filePath: string) {
   try {
-    const res =await fetch(`${env.GO_API_URL}/media`,{
-      method:"DELETE",
-      headers: GoHeaders ,
-      body:JSON.stringify({key:filePath})
-    })
-    if (!res.ok){
-      throw new Error("Failed to delete"+res.statusText)
+    const res = await fetch(`${env.GO_API_URL}/media`, {
+      method: "DELETE",
+      headers: GoHeaders,
+      body: JSON.stringify({ key: filePath }),
+    });
+    if (!res.ok) {
+      throw new Error("Failed to delete" + res.statusText);
     }
   } catch (error) {
-    logger.error("error deleting",error)
-      throw new Error("Failed to delete")
+    logger.error("error deleting", error);
+    throw new Error("Failed to delete");
+  }
+}
+export async function downloadFileFromR2(filePath: string) {
+  try {
+    const res = await fetch(
+      `${env.GO_API_URL}/media/download?key=${filePath}`,
+      {
+        method: "GET",
+        headers: GoHeaders,
+      }
+    );
+    if (!res.ok) {
+      throw new Error("Failed to download," + res.statusText);
+    }
+
+    const arrayBuffer = await res.arrayBuffer();
+    const fileName = filePath.split("/").pop() || "downloaded-file";
+    return { arrayBuffer, fileName };
+
+  } catch (error) {
+    logger.error("error download,", error);
+    throw new Error("Failed to download");
   }
 }
 
@@ -30,11 +51,7 @@ type UploadOptions = {
   url: string;
 };
 
-export async function uploadFiles({
-  files,
-  scope,
-  url ,
-}: UploadOptions) {
+export async function uploadFiles({ files, scope, url }: UploadOptions) {
   const formData = new FormData();
   files.forEach((file) => {
     formData.append("files", file);
@@ -42,7 +59,7 @@ export async function uploadFiles({
   formData.append("scope", scope);
   try {
     const response = await fetch(url, {
-      headers: GoHeaders ,
+      headers: GoHeaders,
       method: "POST",
       body: formData,
     });
@@ -51,7 +68,7 @@ export async function uploadFiles({
       const errorData = await response
         .json()
         .catch(() => ({ message: "Unknown error" }));
-        logger.error("error uploading file", JSON.stringify(errorData))
+      logger.error("error uploading file", JSON.stringify(errorData));
       throw new Error(
         errorData.message || `HTTP error! status: ${response.status}`
       );
@@ -59,7 +76,7 @@ export async function uploadFiles({
     const data = await response.json();
     return data;
   } catch (error) {
-    logger.error("Error in uploadFiles:", {error:error});
+    logger.error("Error in uploadFiles:", { error: error });
     throw error;
   }
 }
