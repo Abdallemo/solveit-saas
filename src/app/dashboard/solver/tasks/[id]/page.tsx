2960605bdaa597690e5cc999ac5966b1a85360e5
EngCamo <learn3038it@gmail.com>
2025-10-21 08:57:54 +0800

    refactor: switch DisplayListComponent to client-side data flow w/ custom useQueryParam + react-query Integrated custom useQueryParam manager to handle URL state sync without Next.js router.refresh. Reworked DisplayListComponent to fetch data client-side via useQuery for smoother state updates, removing SSR dependency and avoiding unnecessary reloads.

--- src/app/dashboard/solver/tasks/[id]/page.tsx
+++ src/app/dashboard/solver/tasks/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { AuthGate } from "@/components/GateComponents";
 import { getServerUserSession } from "@/features/auth/server/actions";
-import TaskPageComps from "@/features/tasks/components/TaskPageComps";
+import SolverTaskPageComps from "@/features/tasks/components/solver/SolverTaskPageComps";
 import { getTasksbyIdWithFiles } from "@/features/tasks/server/data";
 export default async function Page({
   params,
@@ -11,7 +11,10 @@ export default async function Page({
   const currentUser = await getServerUserSession();
   if (!currentUser || !currentUser.id) return <AuthGate />;
 
-  const task = await getTasksbyIdWithFiles(id);
-
-  return <TaskPageComps currentUser={currentUser} task={task} />;
+  const task = await getTasksbyIdWithFiles(id, "SOLVER");
+  const isBlocked = task ? (task.blockedSolvers ? true : false) : false;
+ 
+  return (
+    <SolverTaskPageComps currentUser={currentUser} task={task} isBlocked={isBlocked} />
+  );
 }
