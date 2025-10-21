169104965d0c6ec3a4f27dce70e3083b281edd25
EngCamo <learn3038it@gmail.com>
2025-10-21 08:57:55 +0800

    refactor: switch DisplayListComponent to client-side data flow w/ custom useQueryParam + react-query Integrated custom useQueryParam manager to handle URL state sync without Next.js router.refresh. Reworked DisplayListComponent to fetch data client-side via useQuery for smoother state updates, removing SSR dependency and avoiding unnecessary reloads.

--- src/hooks/useAutoDraftSave.tsx
+++ src/hooks/useAutoDraftSave.tsx
@@ -1,19 +1,21 @@
 "use client";
 
-import { useEffect, useRef } from "react";
 import { debounce as lodashDebounce } from "lodash";
+import { useEffect, useRef } from "react";
 
-type UseAutoSaveProps<T extends any[],RT > = {
+type UseAutoSaveProps<T extends any[], RT> = {
   autoSaveFn: (...args: T) => RT | Promise<RT>;
   autoSaveArgs: T;
   delay?: number;
+  disabled?: boolean;
 };
 
-export function useAutoSave<T extends any[],RT>({
+export function useAutoSave<T extends any[], RT>({
   autoSaveFn,
   autoSaveArgs,
   delay = 2000,
-}: UseAutoSaveProps<T,RT>) {
+  disabled = false,
+}: UseAutoSaveProps<T, RT>) {
   const debouncedAutoSave = useRef(
     lodashDebounce((...args: T) => {
       autoSaveFn(...args);
@@ -24,11 +26,11 @@ export function useAutoSave<T extends any[],RT>({
     const isValidArgs = autoSaveArgs.every(
       (arg) => arg !== undefined && arg !== null
     );
-
+    if (disabled) return;
     if (!isValidArgs) return;
 
     debouncedAutoSave(...autoSaveArgs);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [...autoSaveArgs, debouncedAutoSave]);
 
   useEffect(() => {
