0e5937e37e79485325770dc320a9fadbf98590d9
EngCamo <learn3038it@gmail.com>
2025-10-21 08:57:54 +0800

    refactor: switch DisplayListComponent to client-side data flow w/ custom useQueryParam + react-query Integrated custom useQueryParam manager to handle URL state sync without Next.js router.refresh. Reworked DisplayListComponent to fetch data client-side via useQuery for smoother state updates, removing SSR dependency and avoiding unnecessary reloads.

--- src/app/dashboard/solver/assignedTasks/page.tsx
+++ src/app/dashboard/solver/assignedTasks/page.tsx
@@ -1,54 +1,29 @@
-import { TaskStatusType } from "@/drizzle/schemas";
-import { getServerUserSession } from "@/features/auth/server/actions";
+import { isAuthorized } from "@/features/auth/server/actions";
 import DisplayListComponent from "@/features/tasks/components/DisplayComponent";
-import { handleTaskDeadline } from "@/features/tasks/server/action";
-import {
-  getAllCategoryMap,
-  getAssignedTasksbyIdPaginated,
-} from "@/features/tasks/server/data";
+import { getAllCategoryMap } from "@/features/tasks/server/data";
 
 export default async function ServerWrapper({
   searchParams,
 }: {
   searchParams: Promise<{ search: string; page: string; status: string }>;
 }) {
-  const currentUser = await getServerUserSession();
-  if (!currentUser || !currentUser.role || !currentUser.id) return;
+  const { user } = await isAuthorized(["SOLVER"]);
 
   const categoryMap = await getAllCategoryMap();
-  const { search, page,status } = await searchParams;
-
-  const pages = Number.parseInt(page ?? "1");
   const limit = 8;
-  const offset = (pages - 1) * limit;
 
-  const { tasks, totalCount } = await getAssignedTasksbyIdPaginated(
-    currentUser.id,
-    {
-      search:search ?? "",
-      limit,
-      offset,
-      status: status as TaskStatusType ?? "",
-    },
-    true
-  );
-  for (const task of tasks) {
-    await handleTaskDeadline(task);
-  }
-  const totalPages = Math.ceil(totalCount / limit);
-  const hasPrevious = pages > 1;
-  const hasNext = pages < totalPages;
+  // for (const task of tasks) {
+  //   await handleTaskDeadline(task);
+  // }
+
   return (
     <DisplayListComponent
       title="Assigned Tasks"
-      itretable={tasks}
-      totalCount={totalCount}
       categoryMap={categoryMap}
-      pages={pages}
-      totalPages={totalPages}
-      hasPrevious={hasPrevious}
-      hasNext={hasNext}
       filterType="status"
+      type="SolverTasks"
+      currentUser={user}
+      limit={limit}
     />
   );
 }
