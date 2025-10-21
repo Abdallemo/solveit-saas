c2e3fd1a417b0bdce6cf1bcc9df0816f4b9a41ca
EngCamo <learn3038it@gmail.com>
2025-10-21 08:57:54 +0800

    refactor: switch DisplayListComponent to client-side data flow w/ custom useQueryParam + react-query Integrated custom useQueryParam manager to handle URL state sync without Next.js router.refresh. Reworked DisplayListComponent to fetch data client-side via useQuery for smoother state updates, removing SSR dependency and avoiding unnecessary reloads.

--- src/app/dashboard/admin/ai/page.tsx
+++ src/app/dashboard/admin/ai/page.tsx
@@ -1,7 +1,9 @@
 import { getAllAiRules } from "@/features/Ai/server/action";
 import { AIRuleManagement } from "@/features/Ai/server/components/ai-rule-management";
+import { isAuthorized } from "@/features/auth/server/actions";
 
 export default async function AIPage() {
+  const { user } = await isAuthorized(["ADMIN"]);
   const allAiRules = await getAllAiRules();
   return (
     <div className="h-full w-full flex flex-col flex-1 p-6 items-center gap-3">
@@ -10,7 +12,8 @@ export default async function AIPage() {
         {" "}
         Manage AI rules and policies for your system.
       </p>
-      <AIRuleManagement allAiRules={allAiRules} />
+      
+      <AIRuleManagement allAiRules={allAiRules} adminId={user.id} />
     </div>
   );
 }
