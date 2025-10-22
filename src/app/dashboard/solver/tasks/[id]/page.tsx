32d4b25fcdf4d0655a199fe7070841bf6c12d957
EngCamo <learn3038it@gmail.com>
2025-10-23 06:50:23 +0800

    fixed some bugs regarding search params and shifted to route handlers due to server action issue

--- src/app/dashboard/solver/tasks/[id]/page.tsx
+++ src/app/dashboard/solver/tasks/[id]/page.tsx
@@ -1,5 +1,6 @@
-import { AuthGate } from "@/components/GateComponents";
-import { getServerUserSession } from "@/features/auth/server/actions";
+import {
+  isAuthorized
+} from "@/features/auth/server/actions";
 import SolverTaskPageComps from "@/features/tasks/components/solver/SolverTaskPageComps";
 import { getTasksbyIdWithFiles } from "@/features/tasks/server/data";
 export default async function Page({
@@ -8,13 +9,16 @@ export default async function Page({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  const currentUser = await getServerUserSession();
-  if (!currentUser || !currentUser.id) return <AuthGate />;
+  const { user: currentUser } = await isAuthorized(["SOLVER"]);
 
   const task = await getTasksbyIdWithFiles(id, "SOLVER");
   const isBlocked = task ? (task.blockedSolvers ? true : false) : false;
- 
+
   return (
-    <SolverTaskPageComps currentUser={currentUser} task={task} isBlocked={isBlocked} />
+    <SolverTaskPageComps
+      currentUser={currentUser}
+      task={task}
+      isBlocked={isBlocked}
+    />
   );
 }
