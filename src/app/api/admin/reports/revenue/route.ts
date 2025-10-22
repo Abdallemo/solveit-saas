fd398d9019e917e81fbacf14d6d7db7854ea3e69
EngCamo <learn3038it@gmail.com>
2025-10-23 06:50:24 +0800

    fixed some bugs regarding search params and shifted to route handlers due to server action issue

---
+++ src/app/api/admin/reports/revenue/route.ts
@@ -0,0 +1,15 @@
+import { isAuthorized } from "@/features/auth/server/actions";
+import { getRevenueData } from "@/features/tasks/server/data";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const { user, Auth } = await isAuthorized(["ADMIN"], { useResponse: true });
+  if (Auth.isAuthError) {
+    return Auth.response;
+  }
+
+  let from = req.nextUrl.searchParams.get("from");
+  let to = req.nextUrl.searchParams.get("to");
+  const data = await getRevenueData(from!, to!);
+  return NextResponse.json(data);
+}
