624b9b9e5f67274923b6ed219cfab848cc8a7fde
EngCamo <learn3038it@gmail.com>
2025-10-23 06:50:24 +0800

    fixed some bugs regarding search params and shifted to route handlers due to server action issue

--- src/features/tasks/client/queries.ts
+++ src/features/tasks/client/queries.ts
@@ -7,7 +7,7 @@ import {
 import { UserDbType } from "@/features/users/server/actions";
 import { queryOptions } from "@tanstack/react-query";
 
-type QueryParams = {
+type tasksQueryParams = {
   title: string;
   search: string;
   selectedValue: string;
@@ -19,7 +19,7 @@ type QueryParams = {
   categoryMap: Record<string, string>;
 };
 
-export const tasksQuery = (params: QueryParams) =>
+export const tasksQuery = (params: tasksQueryParams) =>
   queryOptions({
     queryKey: [
       "tasks",
@@ -79,5 +79,112 @@ export const tasksQuery = (params: QueryParams) =>
         },
         true
       );
+    },
+  });
+type userGrowthReturnType = {
+  date: string;
+  users: number;
+}[];
+export const userGrowthQuery = (params: {
+  from: string;
+  to: string;
+  enabled?: boolean;
+}) =>
+  queryOptions({
+    queryKey: ["user-growth", params.from, params.to],
+    queryFn: async () => {
+      let dataToSend: userGrowthReturnType = [];
+      const { from, to } = params;
+      // const { from, to } = params;
+      // console.log("userGrowth key", ["user-growth", from, to]);
+      // const res = await getUserGrowthData(from, to); server action version
+      // console.log("got response", res);
+      try {
+        const res = await fetch(
+          `/api/admin/reports/user_growth?from=${from}&to=${to}`,
+          {
+            method: "GET",
+          }
+        );
+        dataToSend = await res.json();
+
+        return dataToSend;
+      } catch (error) {
+        console.log(error);
+        return dataToSend;
+      }
+    },
+  });
+
+type revenueReturnType = {
+  date: string;
+  totalRevenue: number;
+}[];
+export const revenueQuery = (params: {
+  from: string;
+  to: string;
+  enabled?: boolean;
+}) =>
+  queryOptions({
+    queryKey: ["revenue", params.from, params.to],
+    queryFn: async () => {
+      const { from, to } = params;
+      let revenueToSend: revenueReturnType = [];
+
+      // const { from, to } = params;
+      // console.log("getRevenueData key", ["revenue", from, to]);
+      // const res = await getRevenueData(from, to);//server action version
+      // console.log("got response", res);
+      try {
+        const res = await fetch(
+          `/api/admin/reports/revenue?from=${from}&to=${to}`,
+          {
+            method: "GET",
+          }
+        );
+        revenueToSend = await res.json();
+
+        return revenueToSend;
+      } catch (error) {
+        console.log(error);
+
+        return revenueToSend;
+      }
+    },
+  });
+type taskCategoriesReturnType = {
+  date: string;
+  name: string;
+  taskCount: number;
+}[];
+export const taskCategoriesQuery = (params: {
+  from: string;
+  to: string;
+  enabled?: boolean;
+}) =>
+  queryOptions({
+    queryKey: ["task-categories", params.from, params.to],
+    queryFn: async () => {
+      const { from, to } = params;
+      let taskCategoryToSend: taskCategoriesReturnType = [];
+
+      // const { from, to } = params;
+      // console.log("taskCategoriesQuery key", ["task-categories", from, to]);
+      // const res = await getTaskCategoriesData(from, to);//server action verison
+      // console.log("got response", res);
+      try {
+        const res = await fetch(
+          `/api/admin/reports/task_categories?from=${from}&to=${to}`,
+          {
+            method: "GET",
+          }
+        );
+        taskCategoryToSend = await res.json();
+        return taskCategoryToSend;
+      } catch (error) {
+        console.log(error);
+
+        return taskCategoryToSend;
+      }
     },
   });
