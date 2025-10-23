98fe75708238665807a0076be6b2b229d378f0b4
EngCamo <learn3038it@gmail.com>
2025-10-24 07:51:45 +0800

    fixed the deadline calculation Issue added deadline cron job to auto block solvers fixed file upload ux issues unified go microservice migration with nextjs verison

--- src/features/tasks/components/richTextEdito/Tiptap.tsx
+++ src/features/tasks/components/richTextEdito/Tiptap.tsx
@@ -1,19 +1,22 @@
-"use client"
-import { useEditor, EditorContent } from "@tiptap/react"
-import StarterKit from "@tiptap/starter-kit"
-import Image from "@tiptap/extension-image"
-import TextAlign from "@tiptap/extension-text-align"
-import { common, createLowlight } from "lowlight"
-import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight"
-import Highlight from "@tiptap/extension-highlight"
-import MenuBar from "./MenuBar"
-import { NewuseTask } from "@/contexts/TaskContext"
+"use client";
+import { NewuseTask } from "@/contexts/TaskContext";
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import Highlight from "@tiptap/extension-highlight";
+import Image from "@tiptap/extension-image";
+import TextAlign from "@tiptap/extension-text-align";
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { common, createLowlight } from "lowlight";
+import MenuBar from "./MenuBar";
 
 export default function TaskPostingEditor() {
   // const { content, setContent } = useTask()// migrated
-  const {draft:{content},updateDraft} = NewuseTask()
+  const {
+    draft: { content },
+    updateDraft,
+  } = NewuseTask();
 
-  const lowlight = createLowlight(common)
+  const lowlight = createLowlight(common);
   const editor = useEditor({
     extensions: [
       StarterKit.configure({
@@ -41,23 +44,26 @@ export default function TaskPostingEditor() {
       Image,
     ],
     onUpdate({ editor }) {
-      updateDraft({content:editor.getHTML()})
+      updateDraft({ content: editor.getHTML() });
     },
     content: content,
     immediatelyRender: false,
     editorProps: {
       attributes: {
-        class: "w-full h-full p-14 focus:outline-none",
+        class: "w-full h-full p-14 focus:outline-none break-all",
       },
     },
-  })
+  });
 
   return (
-    <div className="border rounded-md flex flex-col h-[600px] md:h-[800px] lg:h-[800px]" >
+    <div className="border rounded-md flex flex-col h-[600px] md:h-[800px] lg:h-[800px]">
       <MenuBar editor={editor} />
       <div className="flex-1 overflow-hidden">
-        <EditorContent editor={editor} className="h-full overflow-y-auto " />
+        <EditorContent
+          editor={editor}
+          className="h-full overflow-y-auto break-all"
+        />
       </div>
     </div>
-  )
+  );
 }
