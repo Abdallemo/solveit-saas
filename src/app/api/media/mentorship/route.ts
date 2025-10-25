2a4a71a82806899b7d4cdeedce8e9dd63f3f4eb7
EngCamo <learn3038it@gmail.com>
2025-10-25 11:33:58 +0800

    fixed file deleting issue by completly ditching server actions and using route handlers

---
+++ src/app/api/media/mentorship/route.ts
@@ -0,0 +1,50 @@
+import db from "@/drizzle/db";
+import {
+  MentorshipChatFilesTable
+} from "@/drizzle/schemas";
+import { env } from "@/env/server";
+import { isAuthorized } from "@/features/auth/server/actions";
+import { deleteFileFromChatSessionDb } from "@/features/mentore/server/action";
+import { GoHeaders } from "@/lib/go-config";
+import { eq } from "drizzle-orm";
+import { NextRequest } from "next/server";
+
+export async function DELETE(req: NextRequest) {
+  const { user, Auth } = await isAuthorized(["POSTER", "SOLVER"], {
+    useResponse: true,
+  });
+  if (Auth.isAuthError) {
+    return Auth.response;
+  }
+
+  const key = req.nextUrl.searchParams.get("key");
+  if (!key) {
+    return new Response("Missing key", { status: 400 });
+  }
+  try {
+    const file = await db.query.MentorshipChatFilesTable.findFirst({
+      where: eq(MentorshipChatFilesTable.filePath, key),
+      with: {
+        chat: { columns: { sessionId: true } },
+      },
+    });
+
+    if (!file) {
+      return new Response("File not found", { status: 400 });
+    }
+    const res = await fetch(`${env.GO_API_URL}/media`, {
+      method: "DELETE",
+      headers: GoHeaders,
+      body: JSON.stringify({ key: file.filePath }),
+    });
+
+    if (!res.ok || !res.body) {
+      return new Response("Failed to delete file", { status: res.status });
+    }
+    await deleteFileFromChatSessionDb(file.id, user?.id!);
+
+    return new Response("Successfully Deleted", { status: res.status });
+  } catch (error) {
+    return new Response("Failed to delete file", { status: 500 });
+  }
+}
