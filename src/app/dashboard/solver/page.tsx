ba556361aba3d7b265015fd0dae7ba0d4559bb89
EngCamo <learn3038it@gmail.com>
2025-10-25 18:06:23 +0800

    fixed file deleting issue by completly ditching server actions and using route handlers

--- src/app/dashboard/solver/page.tsx
+++ src/app/dashboard/solver/page.tsx
@@ -1,17 +1,12 @@
 import { isAuthorized } from "@/features/auth/server/actions";
-import {
-  getSolverStats,
-  getSolverUpcomminDeadlines,
-} from "@/features/tasks/server/data";
 import SolverDashboard from "@/features/users/components/solver/SolverDashboard";
 
 export default async function page() {
   await isAuthorized(["SOLVER"]);
-  const data = await getSolverStats();
-  const deadlines = await getSolverUpcomminDeadlines();
+
   return (
     <div className="w-full h-full">
-      <SolverDashboard stats={data} deadlines={deadlines} />
+      <SolverDashboard />
     </div>
   );
 }
