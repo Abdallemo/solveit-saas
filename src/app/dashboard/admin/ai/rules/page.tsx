be6969f1202e6554ebc621e92713ce6fef20006b
EngCamo <learn3038it@gmail.com>
2025-10-27 01:17:51 +0800

    Fix: Prevent false moderation flags on technical review Feat: Add critical exception to moderation prompt Refactor(AI): Tune moderation logic and clean up API

---
+++ src/app/dashboard/admin/ai/rules/page.tsx
@@ -0,0 +1,20 @@
+import { getAllAiRules } from "@/features/Ai/server/action";
+import { AIRuleManagement } from "@/features/Ai/server/components/ai-rule-management";
+import { isAuthorized } from "@/features/auth/server/actions";
+
+export default async function AIPage() {
+  const { user } = await isAuthorized(["ADMIN"]);
+  const allAiRules = await getAllAiRules();
+  if (!allAiRules) return;
+  return (
+    <div className="h-full w-full flex flex-col flex-1 p-6 items-center gap-3">
+      <h1 className="text-3xl font-bold tracking-tight">AI Rule Management</h1>
+      <p className="text-muted-foreground">
+        {" "}
+        Manage AI rules and policies for your system.
+      </p>
+
+      <AIRuleManagement allAiRules={allAiRules} adminId={user.id} />
+    </div>
+  );
+}
