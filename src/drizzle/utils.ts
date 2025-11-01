cc057cfb4e1bb197cc3f0f3e5a691abce3e925ad
EngCamo <learn3038it@gmail.com>
2025-11-01 21:20:35 +0800

    Enhaced Query Stats .

--- src/drizzle/utils.ts
+++ src/drizzle/utils.ts
@@ -7,6 +7,10 @@ export function pgFormatDateYMD<
   return sql<string>`to_char(${column}, 'YYYY-MM-DD')`;
 }
 
+export function pgFormatDateExpr(expr: SQL): SQL<string> {
+  return sql<string>`to_char(${expr}::date, 'YYYY-MM-DD')`;
+}
+
 export function pgInterveralRange<
   T extends AnyPgColumn & { dataType: "date" | "timestamp" }
 >(column: T, range: string): SQL<boolean> {
@@ -25,4 +29,42 @@ export function pgCountWithinRange<
   return sql<number>`COUNT(*) FILTER (WHERE ${column} > NOW() - INTERVAL '${sql.raw(
     range
   )}')`;
+}
+export function pgCountWithFilter<
+  T extends AnyPgColumn,
+  V extends T["_"]["data"] 
+>(column: T, value: V): SQL<number> {
+  return sql<number>`COUNT(*) FILTER (WHERE ${column} = ${value})`;
+}
+export function pgWithFilter<
+  T extends AnyPgColumn,
+  V extends T["_"]["data"] 
+>(column: T, value: V): SQL<boolean> {
+  return sql<boolean>`FILTER (WHERE ${column} = ${value})`;
+}
+
+// export function pgGenerateSeries(range: string): SQL {
+//   return sql`
+//     generate_series(
+//       (current_date - interval ${sql.raw(`'${range}'`)})::date,
+//       current_date::date,
+//       interval '1 day'
+//     ) as d
+//   `;
+// }
+
+
+export function pgGenerateSeries(range: string) {
+  const table = sql`
+    generate_series(
+      (current_date - interval ${sql.raw(`'${range}'`)})::date,
+      current_date::date,
+      interval '1 day'
+    ) as days(date)
+  `;
+
+  return {
+    days:table,
+    date: sql<string>`days.date`,
+  };
 }
