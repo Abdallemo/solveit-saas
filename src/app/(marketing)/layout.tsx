c0b4c1a4685af18315f8697c752242e91747b3a9
EngCamo <learn3038it@gmail.com>
2025-11-17 23:34:35 +0800

    added static pages .

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/marketing/navbar";
+import { ReactNode } from "react";
+
+export default function MarketingLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center scroll-smooth">
+      <div className="z-10 w-full max-w-7xl px-4 ">
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
+}
