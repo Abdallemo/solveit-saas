267bc8d52090d62b8128b13572fe2714992c812c
EngCamo <learn3038it@gmail.com>
2025-11-18 10:57:22 +0800

    Major update added image uploading on the editor and more editor options applied to different part of the app successfully .

--- src/features/tasks/components/TaskPageComps.tsx
+++ src/features/tasks/components/TaskPageComps.tsx
@@ -1,23 +1,26 @@
-"use client"
+"use client";
 import { FilesTable } from "@/features/media/components/FilesTable";
 import { TaskNotFoundError } from "@/lib/Errors";
 import { Loader2 } from "lucide-react";
 import { Suspense } from "react";
 import { PosterTaskReturn } from "../server/task-types";
-import TaskPreview from "./richTextEdito/TaskPreview";
+import PostingEditor from "./richTextEdito/BlogTiptap";
 
 export default function TaskPageComps({
   task,
 }: {
-  task: PosterTaskReturn|null;
-
+  task: PosterTaskReturn | null;
 }) {
   if (!task) throw new TaskNotFoundError();
   return (
-    <main className="flex flex-col w-full h-full gap-5 items-center p-10">
+    <main className="flex flex-col w-full h-full gap-5 items-center p-10 ">
       <div className="w-full flex flex-col items-end ">
         <Suspense fallback={<Loader2 className="animate-spin w-2" />}>
-          <TaskPreview content={task?.content} />
+          <PostingEditor
+            content={task?.content}
+            editorOptions={{ editable: false }}
+            showMenuBar={false}
+          />
         </Suspense>
       </div>
       <div className="w-full flex flex-col items-center">
