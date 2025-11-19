640e8d4c474e04eb4e597adf9d01653cf1e0bafa
EngCamo <learn3038it@gmail.com>
2025-11-19 18:44:52 +0800

    Implement concurrent job safety and R2 listing fix .

--- server/internal/worker/worker.go
+++ server/internal/worker/worker.go
@@ -1,36 +1,24 @@
 package worker
 
-import "sync"
+import (
+	"github/abdallemo/solveit-saas/internal/api/websocket"
+	"github/abdallemo/solveit-saas/internal/database"
+	"sync"
 
-type Task interface {
-	Process()
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/go-redis/redis/v8"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Worker struct {
+	store   *database.Queries
+	s3      *s3.Client
+	redis   *redis.Client
+	wsNotif *websocket.WsNotification
+	dbConn  *pgxpool.Pool
+	mu      sync.RWMutex
 }
 
-type WorkerPool struct {
-	Tasks        []Task
-	councurrency int
-	tasksChan    chan Task
-	wg           sync.WaitGroup
-}
-
-func (wp *WorkerPool) worker() {
-	for task := range wp.tasksChan {
-		task.Process()
-		wp.wg.Done()
-	}
-
-}
-func (wp *WorkerPool) Run() {
-	wp.tasksChan = make(chan Task, len(wp.Tasks))
-	defer close(wp.tasksChan)
-
-	for i := 0; i < wp.councurrency; i++ {
-		go wp.worker()
-	}
-	wp.wg.Add(len(wp.Tasks))
-
-	for _, task := range wp.Tasks {
-		wp.tasksChan <- task
-	}
-	wp.wg.Wait()
+func NewWorker(store *database.Queries, s3 *s3.Client, redis *redis.Client, wsNotif *websocket.WsNotification, dbConn *pgxpool.Pool) *Worker {
+	return &Worker{store: store, s3: s3, redis: redis, wsNotif: wsNotif, dbConn: dbConn}
 }
