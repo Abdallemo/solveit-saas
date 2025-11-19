9e3f31423b70f03ff6281c3ff53a147f26f84f6e
EngCamo <learn3038it@gmail.com>
2025-11-19 18:44:52 +0800

    Implement concurrent job safety and R2 listing fix .

--- server/internal/queries/query.sql
+++ server/internal/queries/query.sql
@@ -50,3 +50,22 @@ WHERE id = $1;
 -- name: AddAIFlags :exec
 INSERT INTO ai_flags (hashed_content, reason, confidence_score)
 VALUES ($1, $2, $3);
+
+-- name: GetAllTaskFilePaths :many
+SELECT file_path FROM task_files;
+-- name: GetAllWorkspaceFilePaths :many
+SELECT file_path FROM solution_workspace_files;
+-- name: GetAllChatFilePaths :many
+SELECT file_path FROM mentorship_chat_files;
+-- name: GetAllMediaFilePaths :many
+SELECT file_path FROM global_media_files;
+
+-- name: RemoveTaskFile :exec
+DELETE FROM task_files WHERE file_path = $1;
+-- name: RemoveWorkspaceFile :exec
+DELETE FROM solution_workspace_files WHERE file_path = $1;
+-- name: RemoveChatFile :exec
+DELETE FROM mentorship_chat_files WHERE file_path = $1;
+-- name: RemoveMediaFile :exec
+DELETE FROM global_media_files WHERE file_path = $1;
+
