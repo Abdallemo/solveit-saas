61d10163f5b3ac0117523b05908463dad5506637
EngCamo <learn3038it@gmail.com>
2025-11-20 05:56:58 +0800

    breaking changes - fixed editor issue and migrated to json for editor content .

--- src/features/media/components/FileUploadUi.tsx
+++ src/features/media/components/FileUploadUi.tsx
@@ -76,7 +76,7 @@ export default function FileUploadUi({ className }: FileUploadUiProps) {
     await saveDraftTask(
       title,
       description,
-      content,
+      JSON.stringify(content),
       contentText,
       userId,
       category,
