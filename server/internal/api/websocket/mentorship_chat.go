4f7783fb364ed2e2e2facc15e7658b840878fb1b
EngCamo <learn3038it@gmail.com>
2025-11-28 11:43:17 +0800

    breaking changes updated the task clearning threasholds removed the post enpoint for websockets exepct the notificaiton changed websocket to standalone sturcts

---
+++ server/internal/api/websocket/mentorship_chat.go
@@ -0,0 +1,80 @@
+package websocket
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github/abdallemo/solveit-saas/internal/user"
+)
+
+type ChatFile struct {
+	ID              string  `json:"id"`
+	FileName        string  `json:"fileName"`
+	FileType        string  `json:"fileType"`
+	FileSize        float64 `json:"fileSize"`
+	StorageLocation string  `json:"storageLocation"`
+	FilePath        string  `json:"filePath"`
+	UploadedAt      string  `json:"uploadedAt"`
+	UploadedByID    string  `json:"uploadedById"`
+	ChatID          string  `json:"chatId"`
+	Status          string  `json:"status"`
+}
+
+type Chat struct {
+	ID          string          `json:"id"`
+	SessionID   string          `json:"sessionId"`
+	SentBy      string          `json:"sentBy"`
+	SentTo      string          `json:"sentTo"`
+	Message     string          `json:"message"`
+	CreatedAt   string          `json:"createdAt"`
+	ReadAt      string          `json:"readAt"`
+	ChatFile    []ChatFile      `json:"chatFiles"`
+	ChatOwner   user.PublicUser `json:"chatOwner"`
+	MessageType string          `json:"messageType"`
+}
+type WsMentorChat struct {
+	hub               *WsHub
+	chats             []Chat
+	mentorChatChannel chan IncomingMessage
+}
+
+func NewMentorChat(hub *WsHub) *WsMentorChat {
+	s := &WsMentorChat{
+		hub:               hub,
+		chats:             make([]Chat, 0, 1<<10),
+		mentorChatChannel: make(chan IncomingMessage, 100),
+	}
+	go s.listenForMessages()
+	return s
+}
+
+func (s *WsMentorChat) listenForMessages() {
+	for incMsg := range s.mentorChatChannel {
+
+		var chat Chat
+		if err := json.Unmarshal(incMsg.Payload, &chat); err != nil {
+			log.Printf("Chat Unmarshal Failed: %v", err)
+			continue
+		}
+		s.sendToUser(chat.SessionID, chat.SentTo, chat)
+	}
+}
+
+func (s *WsMentorChat) HandleMentorChats(w http.ResponseWriter, r *http.Request) {
+	sessionID := r.URL.Query().Get("session_id")
+	if sessionID == "" {
+		http.Error(w, "Missing session_id", http.StatusBadRequest)
+		return
+	}
+	q := r.URL.Query()
+	q.Set("channel", "chat:"+sessionID)
+	r.URL.RawQuery = q.Encode()
+
+	s.hub.handleWS(w, r, s.mentorChatChannel)
+}
+
+func (s *WsMentorChat) sendToUser(sessionID, sentTo string, msg Chat) {
+	s.hub.sendToChannel(fmt.Sprintf("chat:%s:%s", sessionID, sentTo), msg)
+}
