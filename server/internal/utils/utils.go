7eb608ffe652c2ebcb8450dc9e8cde7d4c75e9a3
EngCamo <learn3038it@gmail.com>
2025-11-28 11:43:16 +0800

    breaking changes updated the task clearning threasholds removed the post enpoint for websockets exepct the notificaiton changed websocket to standalone sturcts

--- server/internal/utils/utils.go
+++ server/internal/utils/utils.go
@@ -4,10 +4,33 @@ package utils
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/joho/godotenv"
 )
 
 func MakeCacheKey(prefix, content string) string {
 	hash := sha256.Sum256([]byte(content))
 	hashStr := hex.EncodeToString(hash[:])
 	return prefix + hashStr
+}
+
+func GetenvWithDefault(key, defaultVal string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultVal
+	}
+	return value
+}
+
+func LoadEnvs() {
+	_, b, _, _ := runtime.Caller(0)
+	basepath := filepath.Join(filepath.Dir(b), "..", "..")
+	dotenvPath := filepath.Join(basepath, ".env")
+	if err := godotenv.Load(dotenvPath); err != nil {
+		log.Println("No .env file found, falling back to system env")
+	}
 }
