5eb82d89a62fcccaf3eb254590f527421d618f26
EngCamo <learn3038it@gmail.com>
2025-12-09 22:49:58 +0800

    added demo vidoe

--- src/components/marketing/logos/drizzle.tsx
+++ src/components/marketing/logos/drizzle.tsx
@@ -199,11 +199,11 @@ export function OpenAIIcon() {
       width={"40px"}
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         <title>OpenAI icon</title>
